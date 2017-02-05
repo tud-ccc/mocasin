f64db43c675aa00d68282da5b31bc6c806021a01
Christian Menard <christian.menard@tu-dresden.de>
2017-02-05 21:01:16 +0100

    main: delete processor.py
    This module is not nedded anymore. The class Process is defined in
    platform.py

--- pytrm/__init__.py
+++ pytrm/__init__.py
@@ -2,7 +2,6 @@ from .application import *
 from .channel import *
 from .mapping import *
 from .platform import *
-from .processor import *
 from .process import *
 from .scheduler import *
 from .system import *
