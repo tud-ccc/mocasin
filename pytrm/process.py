a0454ddda1b8ff7381bc4deec7db15675681cd5b
Christian Menard <christian.menard@tu-dresden.de>
2017-02-06 15:36:49 +0100

    main: Introduce a TraceReader
    The new class abstracts the reading of traces. This makes the
    implementation of the process much cleaner.

--- pytrm/process.py
+++ pytrm/process.py
@@ -5,7 +5,11 @@
 
 
 import logging
-import os
+
+from .trace import ProcessEntry
+from .trace import ReadEntry
+from .trace import WriteEntry
+from .trace import TerminateEntry
 
 from enum import Enum
 
@@ -28,8 +32,9 @@ class Process(object):
     processor = None
     trace = None
     resume = None
+    traceReader = None
 
-    def __init__(self, env, name, channels):
+    def __init__(self, env, name, channels, TraceReaderClass):
         """
         Constructor
 
@@ -46,6 +51,8 @@ class Process(object):
         for c in channels:
             self.channels[c.name] = c
 
+        self.TraceReaderClass = TraceReaderClass
+
     def unblock(self, event):
         assert self.state == ProcessState.Blocked
         log.debug('{0:16}'.format(self.env.now) + ': process ' + self.name +
@@ -61,12 +68,7 @@ class Process(object):
 
     def assignProcessor(self, processor):
         assert self.processor is None
-        assert self.traceDir is not None
         self.processor = processor
-        traceFile = os.path.join(self.traceDir, self.name + '.' +
-                                 processor.type + '.cpntrace')
-        self.trace = open(traceFile, 'r')
-        assert self.trace is not None
 
     def setTraceDir(self, traceDir):
         assert self.traceDir is None
@@ -77,6 +79,14 @@ class Process(object):
         A SimPy process that replays the process trace
         """
 
+        if self.traceReader is None:
+            assert self.traceDir is not None
+            assert self.processor is not None
+
+            self.traceReader = self.TraceReaderClass(self.traceDir,
+                                                     self.name,
+                                                     self.processor)
+
         self.state = ProcessState.Running
 
         if self.resume is None:
@@ -86,178 +96,73 @@ class Process(object):
             log.debug('{0:16}'.format(self.env.now) + ': process ' +
                       self.name + ' resumes execution')
 
-        if self.resume is not None:
-            # retry
-            if self.resume[0] == 'r':
-                num = int(self.resume[3])
-                log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                          self.name + ' reads ' + str(num) +
-                          ' tokens from channel ' + self.resume[1])
-
-                channel = self.channels[self.resume[1]]
-
-                if channel.canConsumeTokens(num):
-                    delay = channel.getDelayConsumeTokens(num)
-                    t = int(delay / float(self.processor.frequency) * 1000000)
-                    yield self.env.timeout(t)
-                    channel.consumeTokens(num)
-                else:
-                    log.debug('                  Not enough tokens in' +
-                              'channel -> block')
-                    self.state = ProcessState.Blocked
-                    channel.event_produce.callbacks.append(self.unblock)
-                    return
-            elif self.resume[0] == 'w':
-                num = int(self.resume[2])
-                log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                          self.name + ' writes ' + str(num) +
-                          ' tokens to channel ' + self.resume[1])
-
-                channel = self.channels[self.resume[1]]
-
-                if channel.canProduceTokens(num):
-                    delay = channel.getDelayProduceTokens(num)
-                    t = int(delay / float(self.processor.frequency) * 1000000)
-                    yield self.env.timeout(t)
-                    channel.produceTokens(num)
-
-                    transferCycles = channel.getDelayTransferTokens(num)
-                    delay = int(transferCycles /
-                                float(self.processor.frequency) * 1000000)
-                    event = self.env.event()
-                    event.channel = channel
-                    event.num = num
-                    event.callbacks.append(self.finishTransfer)
-                    self.env.schedule(event, delay=delay)
-                else:
-                    log.debug('                  Not enough free slots in' +
-                              ' channel -> block')
-                    self.state = ProcessState.Blocked
-                    channel.event_consume.callbacks.append(self.unblock)
-                    return
-            else:
-                assert False, "found an unexpected trace entry"
-
-        self.resume = None
-
         while not self.state == ProcessState.Finished:
-            traceline = self.trace.readline().split(' ')
 
-            entry_type = traceline[0]
+            entry = None
+            if self.resume is None:
+                entry = self.traceReader.getNextEntry()
+            else:
+                entry, self.resume = self.resume, None
 
-            if entry_type == 'm':
-                cycles = int(traceline[2])
+            if isinstance(entry, ProcessEntry):
+                cycles = entry.cycles
                 ticks = int(cycles / float(self.processor.frequency) * 1000000)
                 log.debug('{0:16}'.format(self.env.now) + ': process ' +
                           self.name + ' processes for ' + str(cycles) +
                           ' cycles (' + str(ticks) + ' ticks)')
                 yield self.env.timeout(ticks)
-            elif entry_type == 'r':
-                channel = self.channels[traceline[1]]
+            elif isinstance(entry, ReadEntry):
+                channel = self.channels[entry.channel]
 
-                num = int(traceline[3])
-                processCycles = int(traceline[4])
-                processTicks = int(processCycles /
-                                   float(self.processor.frequency) * 1000000)
-                consumeCycles = channel.getDelayProduceTokens(num)
-                consumeTicks = int(consumeCycles /
-                                   float(self.processor.frequency) * 1000000)
+                cycles = channel.getDelayProduceTokens(entry.tokens)
+                ticks = int(cycles / float(self.processor.frequency) * 1000000)
 
-                # If we know already that we can produce a token, we merge the
-                # delay for processing and produce operation
-                if channel.canConsumeTokens(num):
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' processes for ' +
-                              str(processCycles) + ' cycles (' +
-                              str(processTicks) + ' ticks)')
-                    log.debug('                 ... and then reads ' +
-                              str(num) + ' tokens to channel ' + traceline[1])
+                log.debug('{0:16}'.format(self.env.now) + ': process ' +
+                          self.name + ' reads ' + str(entry.tokens) +
+                          ' tokens from channel ' + entry.channel)
 
-                    yield self.env.timeout(processTicks+consumeTicks)
-                    channel.consumeTokens(num)
+                if channel.canConsumeTokens(entry.tokens):
+                    yield self.env.timeout(ticks)
+                    channel.consumeTokens(entry.tokens)
                 else:
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' processes for ' +
-                              str(processCycles) + ' cycles (' +
-                              str(processTicks) + ' ticks)')
-                    yield self.env.timeout(processTicks)
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' reads ' + str(num) +
-                              ' tokens from channel ' + traceline[1])
+                    log.debug('                  Not enough tokens in ' +
+                              'channel -> block')
+                    self.state = ProcessState.Blocked
+                    self.resume = entry
+                    channel.event_produce.callbacks.append(self.unblock)
+                    return
+            elif isinstance(entry, WriteEntry):
+                channel = self.channels[entry.channel]
 
-                    if channel.canConsumeTokens(num):
-                        yield self.env.timeout(consumeTicks)
-                        channel.consumeTokens(num)
-                    else:
-                        log.debug('                  Not enough tokens in ' +
-                                  'channel -> block')
-                        self.state = ProcessState.Blocked
-                        self.resume = traceline
-                        channel.event_produce.callbacks.append(self.unblock)
-                        return
-
-            elif entry_type == 'w':
-                channel = self.channels[traceline[1]]
-
-                num = int(traceline[2])
-                processCycles = int(traceline[3])
-                processTicks = int(processCycles /
-                                   float(self.processor.frequency) * 1000000)
-                produceCycles = channel.getDelayProduceTokens(num)
+                produceCycles = channel.getDelayProduceTokens(entry.tokens)
                 produceTicks = int(produceCycles /
                                    float(self.processor.frequency) * 1000000)
-                transferCycles = channel.getDelayTransferTokens(num)
+                transferCycles = channel.getDelayTransferTokens(entry.tokens)
                 transferTicks = int(transferCycles /
                                     float(self.processor.frequency) * 1000000)
 
-                # If we know already that we can produce a token, we merge the
-                # delay for processing and produce operation
-                if channel.canProduceTokens(num):
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' processes for ' +
-                              str(processCycles) + ' cycles (' +
-                              str(processTicks) + ' ticks)')
-                    log.debug('                 ... and then writes ' +
-                              str(num) + ' tokens to channel ' + traceline[1])
+                log.debug('{0:16}'.format(self.env.now) + ': process ' +
+                          self.name + ' writes ' + str(entry.tokens) +
+                          ' tokens to channel ' + entry.channel)
 
-                    yield self.env.timeout(processTicks+produceTicks)
-                    channel.produceTokens(num)
+                if channel.canProduceTokens(entry.tokens):
+                    yield self.env.timeout(produceTicks)
+                    channel.produceTokens(entry.tokens)
 
+                    # schedule an event at the end of the transfer
                     event = self.env.event()
                     event.channel = channel
-                    event.num = num
+                    event.num = entry.tokens
                     event.callbacks.append(self.finishTransfer)
-
                     self.env.schedule(event, delay=transferTicks)
                 else:
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' processes for ' +
-                              str(processCycles) + ' cycles (' +
-                              str(processTicks) + ' ticks)')
-
-                    yield self.env.timeout(processTicks)
-
-                    log.debug('{0:16}'.format(self.env.now) + ': process ' +
-                              self.name + ' writes ' + str(num) +
-                              ' tokens to channel ' + traceline[1])
-
-                    if channel.canProduceTokens(num):
-                        yield self.env.timeout(produceTicks)
-                        channel.produceTokens(num)
-
-                        event = self.env.event()
-                        event.channel = channel
-                        event.num = num
-                        event.callbacks.append(self.finishTransfer)
-                        self.env.schedule(event, delay=transferTicks)
-                    else:
-                        log.debug('                  Not enough free slots ' +
-                                  'in channel -> block')
-                        self.state = ProcessState.Blocked
-                        self.resume = traceline
-                        channel.event_consume.callbacks.append(self.unblock)
-                        return
-            elif traceline[0] == 'e':
+                    log.debug('                  Not enough free slots ' +
+                              'in channel -> block')
+                    self.state = ProcessState.Blocked
+                    self.resume = entry
+                    channel.event_consume.callbacks.append(self.unblock)
+                    return
+            elif isinstance(entry, TerminateEntry):
                 self.state = ProcessState.Finished
             else:
                 assert False, "found an unexpected trace entry"
