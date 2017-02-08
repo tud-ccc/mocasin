3100e7e6e6241e4253340cc7071dceaabcad0d4b
Christian Menard <christian.menard@tu-dresden.de>
2017-02-08 17:19:59 +0100

    platform: adopt the tomahawk2 description to new primitives

--- platform/tomahawk2.py
+++ platform/tomahawk2.py
@@ -4,8 +4,9 @@
 # Authors: Christian Menard
 
 
+from pytrm import CommunicationResource
+from pytrm import CostModel
 from pytrm import Memory
-from pytrm import NocPrimitive
 from pytrm import Platform
 from pytrm import Primitive
 from pytrm import Processor
@@ -13,8 +14,49 @@ from pytrm import Processor
 
 class Tomahawk2Platform(Platform):
 
-    def __init__(self):
+    def createRamPrimitive(self, from_, to, via):
+        p = Primitive()
+        p.typename = "global_ram_cp"
+        p.from_ = self.processors[from_]
+        p.to = self.processors[to]
+        p.via = self.memories[via]
+
+        p.consume = CostModel("299+0.128*x")
+        p.produce = CostModel("209.5 + 0.3179*x")
+
+        return p
+
+    def createConsumerPrimitive(self, from_, to, via):
+        p = Primitive()
+        p.typename = "consumer_cp"
+        p.from_ = self.processors[from_]
+        p.to = self.processors[to]
+        p.via = self.memories[via]
+
+        p.consume = CostModel("299.2+x/bw", bw=8.0)
+        p.produce = CostModel("164")
+
+        p.produceBandwidth = 8.0
+
+        return p
+
+    def createProducerPrimitive(self, from_, to, via, hops):
+        p = Primitive()
+        p.typename = "producer_cp"
+        p.from_ = self.processors[from_]
+        p.to = self.processors[to]
+        p.via = self.memories[via]
+
+        p.produce = CostModel("205")
+        p.consume = CostModel("242.2+15.4*hops+x/bw", bw=8.0, hops=hops)
+
+        return p
+
+    def __init__(self, env):
         super(Platform, self).__init__()
+        self.env = env
+
+        self.nis = []
 
         for i in range(0, 8):
             processor = Processor("PE" + str(i), 'RISC', 200000000)
@@ -22,49 +64,30 @@ class Tomahawk2Platform(Platform):
             memory = Memory("sp" + str(i), 32768)
             self.memories.append(memory)
 
+            ni = CommunicationResource(env, 'ni' + str(i))
+            self.nis.append(ni)
+
+        self.ram = Memory("ram", 268435456)
+        self.memories.append(self.ram)
+
         for i in range(0, 8):
             for j in range(0, 8):
-                self.primitives.append(Primitive(
-                    "global_ram_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "299+0.128*x", "0", "209.5 + 0.3179*x"))
+                p = self.createRamPrimitive(i, j, 8)
+                self.primitives.append(p)
+                p = self.createConsumerPrimitive(i, j, j)
+                self.primitives.append(p)
 
         for i in range(0, 4):
             for j in range(0, 4):
-                self.primitives.append(NocPrimitive(
-                    "consumer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "299.2+x/bw", "0", "164",
-                    1, 8.0, None, None, None, None))
-                self.primitives.append(NocPrimitive(
-                    "producer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "205", "0", "242.2+15.4*hops+x/bw",
-                    None, None, None, None, 1, 8.0))
+                p = self.createProducerPrimitive(i, j, i, 1)
+                self.primitives.append(p)
 
         for i in range(4, 8):
             for j in range(4, 8):
-                self.primitives.append(NocPrimitive(
-                    "consumer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "299.2+x/bw", "0", "164",
-                    1, 8.0, None, None, None, None))
-                self.primitives.append(NocPrimitive(
-                    "producer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "205", "0", "242.2+15.4*hops+x/bw",
-                    None, None, None, None, 1, 8.0))
+                p = self.createProducerPrimitive(i, j, i, 1)
+                self.primitives.append(p)
 
         for i in range(0, 4):
             for j in range(4, 8):
-                self.primitives.append(NocPrimitive(
-                    "consumer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "299.2+x/bw", "0", "164",
-                    3, 8.0, None, None, None, None))
-                self.primitives.append(NocPrimitive(
-                    "producer_cp", self.processors[i], self.processors[j],
-                    self.memories[i], "205", "0", "242.2+15.4*hops+x/bw",
-                    None, None, None, None, 3, 8.0))
-                self.primitives.append(NocPrimitive(
-                    "consumer_cp", self.processors[j], self.processors[i],
-                    self.memories[j], "299.2+x/bw", "0", "164",
-                    3, 8.0, None, None, None, None))
-                self.primitives.append(NocPrimitive(
-                    "producer_cp", self.processors[j], self.processors[i],
-                    self.memories[j], "205", "0", "242.2+15.4*hops+x/bw",
-                    None, None, None, None, 3, 8.0))
+                p = self.createProducerPrimitive(i, j, i, 3)
+                self.primitives.append(p)
