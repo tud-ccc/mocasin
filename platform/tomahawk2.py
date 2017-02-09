d83806c68dca251e9320fb66c998118d7a34b72d
Christian Menard <christian.menard@tu-dresden.de>
2017-02-10 00:29:58 +0100

    platform: update the tomahawk2 description

--- platform/tomahawk2.py
+++ platform/tomahawk2.py
@@ -15,18 +15,6 @@ from simpy.resources.resource import Resource
 
 class Tomahawk2Platform(Platform):
 
-    def createRamPrimitive(self, from_, to, via):
-        p = Primitive()
-        p.typename = "global_ram_cp"
-        p.from_ = self.processors[from_]
-        p.to = self.processors[to]
-        p.via = self.memories[via]
-
-        p.consume = CostModel("299+0.128*x")
-        p.produce = CostModel("209.5 + 0.3179*x")
-
-        return p
-
     def createConsumerPrimitive(self, from_, to, via):
         p = Primitive()
         p.typename = "consumer_cp"
@@ -34,28 +22,51 @@ class Tomahawk2Platform(Platform):
         p.to = self.processors[to]
         p.via = self.memories[via]
 
-        p.produce = CostModel("299", bw=8.0)
-        p.producerTransport = CostModel("x/bw", bw=8.0)
-        p.consume = CostModel("164")
+        p.producePrepare = CostModel("299", bw=8.0)
+        p.produceTransport = CostModel("x/bw", bw=8.0)
+        p.consumePrepare = CostModel("164")
 
-        p.producerTransport.resources.append(self.nis[from_])
-        p.producerTransport.resources.append(self.nis[to])
+        p.produceTransport.resources.append(self.nis_out[from_])
+
+        if from_ < 4 and to >= 4:
+            p.produceTransport.resources.append(self.link_10_to_11)
+            p.produceTransport.resources.append(self.link_11_to_01)
+        if from_ >= 4 and to < 4:
+            p.produceTransport.resources.append(self.link_01_to_11)
+            p.produceTransport.resources.append(self.link_11_to_10)
+
+        p.produceTransport.resources.append(self.nis_in[to])
 
         return p
 
-    def createProducerPrimitive(self, from_, to, via, hops):
+    def createProducerPrimitive(self, from_, to, via):
         p = Primitive()
         p.typename = "producer_cp"
         p.from_ = self.processors[from_]
         p.to = self.processors[to]
         p.via = self.memories[via]
 
-        p.produce = CostModel("205")
-        p.consumerTransport = CostModel("15.4*hops+x/bw", bw=8.0, hops=hops)
-        p.consume = CostModel("242")
+        hops = 1
+        if (from_ < 4 and to >= 4) or (from_ >= 4 and to < 4):
+            hops = 3
 
-        p.consumerTransport.resources.append(self.nis[from_])
-        p.consumerTransport.resources.append(self.nis[to])
+        p.producePrepare = CostModel("205")
+        p.consumePrepare = CostModel("242")
+        p.consumeRequest = CostModel("8*hops", bw=8.0, hops=hops)
+        p.consumeTransport = CostModel("8*hops+x/bw", bw=8.0, hops=hops)
+
+        p.consumeRequest.resources.append(self.nis_out[to])
+        p.consumeTransport.resources.append(self.nis_out[from_])
+
+        if from_ < 4 and to >= 4:
+            p.consumeTransport.resources.append(self.link_10_to_11)
+            p.consumeTransport.resources.append(self.link_11_to_01)
+        if from_ >= 4 and to < 4:
+            p.consumeTransport.resources.append(self.link_01_to_11)
+            p.consumeTransport.resources.append(self.link_11_to_10)
+
+        p.consumeRequest.resources.append(self.nis_in[from_])
+        p.consumeTransport.resources.append(self.nis_in[to])
 
         return p
 
@@ -63,7 +74,8 @@ class Tomahawk2Platform(Platform):
         super(Platform, self).__init__()
         self.env = env
 
-        self.nis = []
+        self.nis_in = []
+        self.nis_out = []
 
         for i in range(0, 8):
             processor = Processor("PE" + str(i), 'RISC', 200000000)
@@ -72,29 +84,23 @@ class Tomahawk2Platform(Platform):
             self.memories.append(memory)
 
             ni = Resource(env, capacity=1)
-            self.nis.append(ni)
+            no = Resource(env, capacity=1)
+            self.nis_in.append(ni)
+            self.nis_out.append(no)
 
-        self.ram = Memory("ram", 268435456)
-        self.memories.append(self.ram)
+        self.link_00_to_10 = Resource(env)
+        self.link_10_to_11 = Resource(env)
+        self.link_11_to_01 = Resource(env)
+        self.link_01_to_00 = Resource(env)
+
+        self.link_00_to_01 = Resource(env)
+        self.link_01_to_11 = Resource(env)
+        self.link_11_to_10 = Resource(env)
+        self.link_10_to_00 = Resource(env)
 
         for i in range(0, 8):
             for j in range(0, 8):
-                p = self.createRamPrimitive(i, j, 8)
-                self.primitives.append(p)
                 p = self.createConsumerPrimitive(i, j, j)
                 self.primitives.append(p)
-
-        for i in range(0, 4):
-            for j in range(0, 4):
-                p = self.createProducerPrimitive(i, j, i, 1)
-                self.primitives.append(p)
-
-        for i in range(4, 8):
-            for j in range(4, 8):
-                p = self.createProducerPrimitive(i, j, i, 1)
-                self.primitives.append(p)
-
-        for i in range(0, 4):
-            for j in range(4, 8):
-                p = self.createProducerPrimitive(i, j, i, 3)
+                p = self.createProducerPrimitive(i, j, i)
                 self.primitives.append(p)
