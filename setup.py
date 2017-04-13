35571aa0c3f2a5055bd50874db8f4af06189d445
Christian Menard <christian.menard@tu-dresden.de>
2017-04-13 14:57:59 +0200

    packaging: add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pykpn",
+    version="0.1",
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['simulate_slx=pykpn.simulate.simulate_slx:main'],
+    }
+)
