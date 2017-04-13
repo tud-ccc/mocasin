5931cf2a3cf24e201bd559cb99c60d33924a2268
Christian Menard <christian.menard@tu-dresden.de>
2017-04-13 17:48:58 +0200

    packaging: update package description

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['simulate_slx=pykpn.simulate.simulate_slx:main'],
+        'console_scripts': ['simulate_slx=pykpn.simulate.simulate_slx:main',
+                            'dc_run=pykpn.design_centering.design_centering.designCentering:main']
     },
     install_requires=[
           'argparse',
+          'matplotlib',
           'numpy',
           'pydot',
           'simpy',
