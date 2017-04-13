981f0d96a6aef9f0ca0fbf81423218461aa6e027
Christian Menard <christian.menard@tu-dresden.de>
2017-04-13 15:15:45 +0200

    packaging: list all dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setup(
     name="pykpn",
     version="0.1",
     packages=find_packages(),
-    entry_points = {
+    entry_points={
         'console_scripts': ['simulate_slx=pykpn.simulate.simulate_slx:main'],
-    }
+    },
+    install_requires=[
+          'argparse',
+          'numpy',
+          'pydot',
+          'simpy',
+          'pyvcd',
+      ],
 )
