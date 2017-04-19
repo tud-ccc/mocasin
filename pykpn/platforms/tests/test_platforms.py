b58bcfeec83c53daac614ccd879f0b8acca9c249
Bhaavan Goel <bhaavangoel.beece14@pec.edu.in>
2017-04-19 17:15:48 +0200

    added mesh and torus tests for generic platform

--- pykpn/platforms/tests/test_platforms.py
+++ pykpn/platforms/tests/test_platforms.py
@@ -3,7 +3,7 @@ import simpy
 from unittest import TestCase
 
 from pykpn.platforms import Tomahawk2Platform
-
+from pykpn.platforms import GeneralPlatform
 
 class TestPlatforms(TestCase):
     def test_tomahawk2(self):
@@ -11,3 +11,9 @@ class TestPlatforms(TestCase):
         th2 = Tomahawk2Platform(env)
         # There isn't really anything that we could assert here. If there
         # are no exceptions we are good.
+    def test_generic(self):
+        env= simpy.Environment()
+        th0=GeneralPlatform(env, 'torus', 2, 4)
+        th1=GeneralPlatform(env, 'mesh', 3, 8)
+        th2=GeneralPlatform(env, 'torus', 3, 3)
+        th3=GeneralPlatform(env, 'mesh', 8, 8)
