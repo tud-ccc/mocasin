35cd779a7a84bd51bd9614f0b00c8f7cf0329f5a
Christian Menard <christian.menard@tu-dresden.de>
2017-06-13 16:12:54 +0200

    simulate: apply autopep8 and adopt to changes in common

--- pykpn/simulate/scheduler.py
+++ pykpn/simulate/scheduler.py
@@ -29,21 +29,22 @@ class Scheduler(object):
         self.processes = processes
         self.policy = policy
 
-        self.prev_process=None
-        self.first_iteration=False
+        self.prev_process = None
+        self.first_iteration = False
 
         self.schedulingDelay = info.policies[policy]
 
         self.vcd_writer = system.vcd_writer
         self.process_var = self.vcd_writer.register_var(
-                'system.' + 'schedulers.' + self.name, 'process', 'integer',
-                size=128, init=None)
-        self.wake_up=self.env.event()
+            'system.' + 'schedulers.' + self.name, 'process', 'integer',
+            size=128, init=None)
+        self.wake_up = self.env.event()
 
     def assignProcess(self, process):
         self.processes.append(process)
         process.assignProcessor(self.processor)
-        log.info('{0:16}'.format(self.env.now)+": "+process.name+" added to "+self.name)
+        log.info('{0:16}'.format(self.env.now) + ": " +
+                 process.name + " added to " + self.name)
         self.wake_up.succeed()
         self.wake_up = self.env.event()
 
@@ -52,20 +53,20 @@ class Scheduler(object):
                  self.name + ' starts execution')
         while(True):
             log.info('{0:16}'.format(self.env.now) + ': scheduler ' +
-                 self.name +' woke up')
+                     self.name + ' woke up')
 
             while True:
-                p,allProcessesFinished=self.scheduling()
+                p, allProcessesFinished = self.scheduling()
                 if p is not None:
-                    delay=self.scheduling_delay(p)
+                    delay = self.scheduling_delay(p)
                     yield self.env.timeout(self.processor.ticks(delay))
 
                     self.vcd_writer.change(self.process_var, self.env.now,
-                                            int(p.vcd_id[0:128], 2))
+                                           int(p.vcd_id[0:128], 2))
                     yield self.env.process(p.run())
                 else:
                     self.vcd_writer.change(self.process_var, self.env.now,
-                                            None)
+                                           None)
 
                     if allProcessesFinished:
                         break
@@ -75,64 +76,67 @@ class Scheduler(object):
                     for pro in self.processes:
                         events.append(pro.event_unblock)
                     yield simpy.events.AnyOf(self.env, events)
-            log.info('{0:16}'.format(self.env.now) +': scheduler ' + self.name+" going to sleep")
+            log.info('{0:16}'.format(self.env.now) +
+                     ': scheduler ' + self.name + " going to sleep")
             yield self.wake_up
 
     def scheduling(self):
-        if self.policy=='None':
+        if self.policy == 'None':
             return self.none_sched()
-        elif self.policy=='FIFO':
+        elif self.policy == 'FIFO':
             return self.fifo_sched()
-        elif self.policy=='RoundRobin':
+        elif self.policy == 'RoundRobin':
             return self.roundrobin_sched()
         else:
             raise RuntimeError('The scheduling policy ' + self.policy +
-                                   ' is not supported')
+                               ' is not supported')
 
     def scheduling_delay(self, p):
-        if self.policy=='None':
+        if self.policy == 'None':
             return self.delay_none()
-        elif self.policy=='FIFO':
+        elif self.policy == 'FIFO':
             return self.delay_fifo(p)
-        elif self.policy=='RoundRobin':
+        elif self.policy == 'RoundRobin':
             return self.delay_roundrobin()
         else:
             raise RuntimeError('The scheduling policy ' + self.policy +
-                                   ' is not supported')
+                               ' is not supported')
 
     def none_sched(self):
-        if self.prev_process==None and self.processes:
-            #if prev_process is empty allot a process from the list
-            self.prev_process=self.processes[0]
-        elif self.prev_process==None:
-            #if prev_process is empty and there are no available processes
-            #return None processes and allProcessesFinished is True
-            return None,True
+        if self.prev_process is None and self.processes:
+            # if prev_process is empty allot a process from the list
+            self.prev_process = self.processes[0]
+        elif self.prev_process is None:
+            # if prev_process is empty and there are no available processes
+            # return None processes and allProcessesFinished is True
+            return None, True
         assert not self.prev_process.state == ProcessState.Running
         if self.prev_process.state == ProcessState.Blocked:
-            #if the process is blocked allProcessFinished is false
-            return None,False
+            # if the process is blocked allProcessFinished is false
+            return None, False
         elif self.prev_process.state == ProcessState.Finished:
-            if self.processes.index(self.prev_process)+1==len(self.processes):
-            #the process is finished and there are no more processes
+            if self.processes.index(self.prev_process) + 1 == \
+               len(self.processes):
+                # the process is finished and there are no more processes
                 return None, True
             else:
-            #the processs is finished so allot next process to the previous process
-                self.prev_process=self.processes[self.processes.index(self.prev_process)+1]
-                return self.prev_process,False
+                # the processs is finished so allot next process to the
+                # previous process
+                self.prev_process = self.processes[
+                    self.processes.index(self.prev_process) + 1]
+                return self.prev_process, False
         elif self.prev_process.state == ProcessState.Ready:
             return self.prev_process, False
-
 
     def roundrobin_sched(self):
         allProcessesFinished = True
         for process in self.processes:
             assert not process.state == ProcessState.Running
             if process.state == ProcessState.Blocked:
-            #if the current process is blocked
+                # if the current process is blocked
                 allProcessesFinished = False
             elif process.state == ProcessState.Finished:
-            #if thr current process is finished move on to the next one
+                # if thr current process is finished move on to the next one
                 continue
             elif process.state == ProcessState.Ready:
                 allProcessesFinished = False
@@ -143,8 +147,8 @@ class Scheduler(object):
 
     def fifo_sched(self):
         allProcessesFinished = True
-        min=sys.maxsize
-        p=None
+        min = sys.maxsize
+        p = None
         for process in self.processes:
             if process.state == ProcessState.Blocked:
                 allProcessesFinished = False
@@ -154,11 +158,11 @@ class Scheduler(object):
                 allProcessesFinished = False
 
                 if int(process.time) < min:
-                    #the process that has been waiting the longest
-                    #should be run first
-                    min=int(process.time)
-                    p=process
-                    return p,allProcessesFinished
+                    # the process that has been waiting the longest
+                    # should be run first
+                    min = int(process.time)
+                    p = process
+                    return p, allProcessesFinished
             else:
                 assert False, 'unknown process state'
         return None, allProcessesFinished
@@ -169,26 +173,25 @@ class Scheduler(object):
     def delay_fifo(self, p):
         delay = self.schedulingDelay
         if self.prev_process is None:
-        # we need to load the first process
+            # we need to load the first process
             delay = delay + self.processor.contextSwitchInDelay
         elif p != self.prev_process:
             delay = delay + self.processor.contextSwitchInDelay + \
-                    self.processor.contextSwitchOutDelay
+                self.processor.contextSwitchOutDelay
         self.prev_process = p
         return delay
-
 
     def delay_roundrobin(self):
         delay = self.schedulingDelay
         if self.first_iteration:
             # Nothing to switch out on first iteration
             delay = delay +\
-                    self.processor.contextSwitchInDelay
+                self.processor.contextSwitchInDelay
             self.first_iteration = False
         else:
             delay = delay + \
-                    self.processor.contextSwitchInDelay + \
-                    self.processor.contextSwitchOutDelay
+                self.processor.contextSwitchInDelay + \
+                self.processor.contextSwitchOutDelay
         return delay
 
     def setTraceDir(self, dir):
