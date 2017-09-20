4afb0c5fddcb8c4d9dc35b414cd0b0ef802f6798
Christian Menard <christian.menard@tu-dresden.de>
2017-09-20 18:01:17 +0200

    maps: add a script for generating platform graphs

---
+++ pykpn/slx/platform_to_dot.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017 TU Dresden
+# All Rights Reserved
+#
+# Authors: Christian Menard
+
+import argparse
+import logging
+
+from .platform import SlxPlatform
+
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-v',
+        '--verbosity',
+        action="count",
+        help="increase output verbosity (e.g., -vv is more than -v)",
+        dest='verbosity')
+
+    parser.add_argument('platform', help="xml platform description", type=str)
+    parser.add_argument('dot', help="dot output file", type=str)
+
+    args = parser.parse_args()
+
+    if args.verbosity is not None:
+        if args.verbosity >= 2:
+            logging.basicConfig(level=logging.DEBUG)
+        elif args.verbosity >= 1:
+            logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    print(args.platform)
+    platform = SlxPlatform(args.platform)
+    dot = platform.to_pydot()
+    dot.write_raw(args.dot)
+
+
+if __name__ == '__main__':
+    main()
