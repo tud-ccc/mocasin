e0197429886923f794ddde43ffecf1ea9a3491dc
Christian Menard <christian.menard@tu-dresden.de>
2017-09-21 15:16:42 +0200

    common: add doc strings to KPN classes

--- pykpn/common/kpn.py
+++ pykpn/common/kpn.py
@@ -5,13 +5,33 @@
 
 
 class KpnProcess(object):
+    """Represents a KPN process
+
+    :ivar str name: name of the process
+    :ivar outgoing_channels: list of KPN channel the process writes to
+    :type outgoing_channels: list[KpnChannel]
+    :ivar incoming_channels: list of KPN channel the process reads from
+    :type incoming_channels: list[KpnChannel]
+    """
 
     def __init__(self, name):
+        """Initialize the KPN process
+
+        Sets the name and leaves the process unconnected
+        """
         self.name = name
         self.outgoing_channels = []
         self.incoming_channels = []
 
     def connect_to_outgoing_channel(self, channel):
+        """Connect the process to a outgoing channel
+
+        This makes this process the source of the channel.
+
+        :param KpnChannel channel: the channel to connect to
+        :raises RuntimeError: if the channel already has a source process
+            assigned
+        """
         if channel.source is not None:
             raise RuntimeError('The channel %s is already connected to a '
                                'source process!', channel.name)
@@ -19,13 +39,37 @@ class KpnProcess(object):
         self.outgoing_channels.append(channel)
 
     def connect_to_incomming_channel(self, channel):
+        """Connect the process to a incomming channel
+
+        This makes this process a sink of the channel.
+
+        :param KpnChannel channel: the channel to connect to
+        """
         channel.sinks.append(self)
         self.incoming_channels.append(channel)
 
 
 class KpnChannel(object):
+    """Represents a KPN channel
+
+    Each channel may have multiple sinks but only one source.
+
+    :ivar str name: name of the channel
+    :ivar source: the channel's source process
+    :type source: KpnProcess
+    :ivar sinks: the channel's sink processes
+    :type sinks: list[KpnProcess]
+    :ivar int token_size: size of one data token in byte
+    """
 
     def __init__(self, name, token_size):
+        """Initialize the channel.
+
+        Sets the channel name and token size but leaves the channel
+        unconnected.
+        :param str name: name of the channel
+        :param int token_size: size of one data token in byte
+        """
         self.name = name
         self.source = None
         self.sinks = []
@@ -33,14 +77,29 @@ class KpnChannel(object):
 
 
 class KpnGraph(object):
+    """Represents the DAG of a KPN application.
+
+    :ivar processes: a list of KPN processes representing the graph's nodes
+    :type processes: list[KpnProcess]
+    :ivar channels: a list of KPN channels representing the graph's edges
+    :type channels: list[KpnChannel]
+    """
 
     def __init__(self):
+        """Create an empty graph"""
         self.processes = []
         self.channels = []
 
     def find_process(self, name, throw=False):
+        """Search for a KPN process by its name.
 
+        :param str name: name of the process to be searched for
+        :param bool throw: raise a RuntimeError if no process is
+                           found (default: False)
 
+        :raises RuntimeError: if no process was found and throw is True
+        :returns: A KPN process object or None if no process was found.
+        """
         for p in self.processes:
             if p.name == name:
                 return p
@@ -49,6 +108,15 @@ class KpnGraph(object):
         return None
 
     def find_channel(self, name, throw=False):
+        """Search for a KPN channel by its name.
+
+        :param str name: name of the channel to be searched for
+        :param bool throw: raise a RuntimeError if no channel is
+                           found (default: False)
+
+        :raises RuntimeError: if no channel was found and throw is True
+        :returns: A KPN channel object or None if no channel was found.
+        """
         for c in self.channels:
             if c.name == name:
                 return c
