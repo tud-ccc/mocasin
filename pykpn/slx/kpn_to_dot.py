4a84b626e8f6229b089c957a5c26f13bd52f6525
Christian Menard <christian.menard@tu-dresden.de>
2017-09-21 15:34:44 +0200

    maps: add script that reads KPN and converts to dot

---
+++ pykpn/slx/kpn_to_dot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017 TU Dresden
+# All Rights Reserved
+#
+# Authors: Christian Menard
+
+import argparse
+import logging
+
+from .kpn import SlxKpnGraph
+
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-v',
+        '--verbosity',
+        action="count",
+        help="increase output verbosity (e.g., -vv is more than -v)",
+        dest='verbosity')
+
+    parser.add_argument('kpn', help="xml kpn graph description", type=str)
+    parser.add_argument('dot', help="dot output file", type=str)
+
+    args = parser.parse_args()
+
+    if args.verbosity is not None:
+        if args.verbosity >= 2:
+            logging.basicConfig(level=logging.DEBUG)
+        elif args.verbosity >= 1:
+            logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    kpn = SlxKpnGraph('app', args.kpn)
+    dot = kpn.to_pydot()
+    dot.write_raw(args.dot)
+
+
+if __name__ == '__main__':
+    main()
