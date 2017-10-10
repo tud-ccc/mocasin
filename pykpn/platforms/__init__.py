1a48863695ad382444f99be30a1d25bc18240d51
Christian Menard <christian.menard@tu-dresden.de>
2017-10-10 16:20:12 +0200

    platforms: apply autopep8

--- pykpn/platforms/__init__.py
+++ pykpn/platforms/__init__.py
@@ -1,24 +1,28 @@
-from .tomahawk2 import *
-from .generic_noc import *
+# Copyright (C) 2017 TU Dresden
+# All Rights Reserved
+#
+# Authors: Christian Menard
+
 
 from pykpn.platforms import Tomahawk2Platform
 from pykpn.platforms import GenericNocPlatform
 
 
+# TODO see if we can implement this in a better way
+
 def createPlatformByName(name):
     if name is None:
         raise ValueError('Define the platform')
 
-    elif name[0:name.find('_')]=='generic':
-        temp1=name.find('_')
-        temp2=name[temp1+1:].find('_')
-        arch=name[temp1+1:temp1+temp2+1]
-        x=int(name[-1])
-        y=int(name[-3])
+    elif name[0:name.find('_')] == 'generic':
+        temp1 = name.find('_')
+        temp2 = name[temp1 + 1:].find('_')
+        arch = name[temp1 + 1:temp1 + temp2 + 1]
+        x = int(name[-1])
+        y = int(name[-3])
         return GenericNocPlatform(arch, x, y)
 
-    elif name=='tomahawk2':
+    elif name == 'tomahawk2':
         return (Tomahawk2Platform())
     else:
         raise ValueError('Platform does not exist')
-
