d9ceb193d4394ce2504f3d7f2614fec9fd591d7f
Christian Menard <christian.menard@tu-dresden.de>
2017-10-12 14:10:24 +0200

    maps: add new maps runtime system class
    Upon initialization, this builds a runtime system object from a
    maps configuration object

---
+++ pykpn/slx/system.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2017 TU Dresden
+# All Rights Reserved
+#
+# Authors: Christian Menard
+
+
+from pykpn.simulate.application import RuntimeKpnApplication
+from pykpn.simulate.system import RuntimeSystem
+from pykpn.slx.kpn import SlxKpnGraph
+from pykpn.slx.mapping import SlxMapping
+from pykpn.slx.platform import SlxPlatform
+from pykpn.slx.trace import SlxTraceReader
+
+
+class SlxRuntimeSystem(RuntimeSystem):
+    """SLX specialization of the RuntimeSystem class"""
+
+    def __init__(self, config, env):
+        """Initialize a slx runtime system object
+
+        :param SlxSimulationConfig config: an slx simulation config object
+        :param simpy.Environment env: the simpy environment
+        """
+        version = config.slx_version
+        platform = SlxPlatform(config.platform_xml, config.slx_version)
+        applications = []
+        for app_config in config.applications:
+            name = app_config.name
+            kpn = SlxKpnGraph(name, app_config.cpn_xml)
+            mapping = SlxMapping(
+                kpn, platform, app_config.mapping_xml, version)
+            trace_reader = SlxTraceReader(
+                app_config.trace_dir, '%s.' % (app_config.name))
+            app = RuntimeKpnApplication(name, kpn, mapping, trace_reader, env,
+                                        app_config.start_at_tick)
+            applications.append(app)
+
+        super().__init__(platform, applications, env)
