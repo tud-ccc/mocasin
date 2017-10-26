a14ff32a400d50c35609494d51ae5de99bdee38d
Christian Menard <christian.menard@tu-dresden.de>
2017-10-26 17:24:59 +0200

    simulate: add tests for the channel module

--- pykpn/simulate/test/conftest.py
+++ pykpn/simulate/test/conftest.py
@@ -9,6 +9,8 @@ from unittest.mock import Mock
 import simpy
 
 import pytest
+from pykpn.common.mapping import ChannelMappingInfo
+from pykpn.simulate.channel import RuntimeChannel
 from pykpn.simulate.process import RuntimeKpnProcess, RuntimeProcess
 
 
@@ -25,6 +27,12 @@ def base_process(env):
 @pytest.fixture
 def kpn_process(env):
     return RuntimeKpnProcess('test_proc', Mock(), env, start_at_tick=1000)
+
+
+@pytest.fixture
+def channel(env):
+    info = ChannelMappingInfo(Mock(), 4)
+    return RuntimeChannel('test_chan', info, 8, env)
 
 
 @pytest.fixture
