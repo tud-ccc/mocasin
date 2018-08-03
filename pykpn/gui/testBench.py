7ab5676cf8ab8846060c47bf9ac80bafa412c54c
Felix Teweleit <felix.teweleit@yahoo.de>
2018-08-03 13:34:43 +0200

    deleted the old gui script with the bad non class based structure. started work on new methods to recognize and draw a network on chip

--- pykpn/gui/testBench.py
+++ pykpn/gui/testBench.py
@@ -4,6 +4,33 @@
 this file is just for purposes of debugging and will be removed later
 '''
 import os
+import sys
+sys.path.append('../..')
+import os
 
-print('lol')
-print(os.path.basename(__file__))
+from pykpn.gui.listOperations import listOperations
+from pykpn.gui.platformOperations import platformOperations
+from pykpn.slx.platform import SlxPlatform
+
+
+
+platform = SlxPlatform('SlxPlatform', '/net/home/teweleit/eclipseWorkspace/pykpn/pykpn/apps/audio_filter/exynos/exynos.platform', '2017.04')
+print(platform.processors())
+listToPrint = platformOperations.findEqualPrimitives(platformOperations, platform)
+for item in listToPrint:
+    print(item)
+
+'''
+for entry in platform.to_adjacency_dict():
+    minimalCost = 1000000000000000
+    peWithMinimalCost = []
+    for tupel in platform.to_adjacency_dict()[entry]:
+        if tupel[1] < minimalCost and tupel[1] > 0:
+            minimalCost = tupel[1]
+            peWithMinimalCost = []
+        if tupel[1] == minimalCost:
+            peWithMinimalCost.append(tupel[0])
+    print(entry)
+    print(minimalCost)
+    print(peWithMinimalCost)
+'''
