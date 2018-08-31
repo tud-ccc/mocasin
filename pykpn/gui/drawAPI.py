061bcab3732ce93ca6b3176aa06d46236ca68396
Felix Teweleit <felix.teweleit@yahoo.de>
2018-08-31 16:23:59 +0200

    added cool new features, multiple applications, adding and removing of mappings via ID, setting own colors, everything is implemented in the drawAPI interface for easy use!

--- pykpn/gui/drawAPI.py
+++ pykpn/gui/drawAPI.py
@@ -5,58 +5,37 @@ created by Felix Teweleit
 
 from pykpn.gui.listOperations import listOperations
 from pykpn.gui.platformOperations import platformOperations
-from textwrap import fill
 
 class drawAPI():
     '''
     drawManager must be a canvas object which should draw the elements, height and width of canvas will be draw height and width, border
-    is the space in pxl between the drawn elements and scaling is the factor how high the elements will be drawn, eg. a scaling of 15 
-    means each element will take 1/15 of the remaining height, width and height should match width and height of the canvas
+    is the space in pixel between the drawn elements and scaling is the factor how high the elements will be drawn, e.g. a scaling of 15 
+    means each element will take 1/15 of the remaining height, width and height should match width and height of the canvas, platform-
+    and mappingColor are optional parameters which represent a list of color names or codes in python
     '''
-    def __init__(self, drawDevice, border, scaling, width, height):
+    def __init__(self, drawDevice, border, scaling, width, height, platformColors = ['default'], mappingColors = ['default']):
         self.drawDevice = drawDevice
         self.innerBorder = border
         self.outerBorder = border
         self.scaling = scaling
         self.drawHeight = height
         self.drawWidth  = width
-        self.colorDict = {1:'aliceblue', 
-                          2:'aqua', 
-                          3:'azure1', 
-                          4:'yellow', 
-                          5:'bisque1', 
-                          6:'blue', 
-                          7:'blueviolet', 
-                          8:'brown1', 
-                          9:'burlywood',
-                          10: 'cadetblue',
-                          11: 'orange',
-                          12: 'chartreuse1',
-                          13: 'chocolate',
-                          14: 'cornsilk2',
-                          15: 'coral',
-                          16: 'cornflowerblue',
-                          17: 'cyan2',
-                          18: 'darkgoldenrod1',
-                          19: 'darkorange',
-                          20: 'darkorchid'}
+        self.platformColors = {}                    #holds the available colors for platform elements
+        self.mappingColors = {}                     #holds the available colors for mapping elements
         self.platformDescription = []               #holds the description of the platform which should be drawn
-        self.mappingDescription = {}                #not implemented yet
+        self.mappingDescription = {}                #holds the description of the currently applied mappings
         self.equalList = []                         #a list of lists of primitives which connect the same processing elements
-        self.coreDict = {}                          #dict, which will hold PE name, start and end points, will be updated during draw Process
+        self.coreDict = {}                          #dictionary, which will hold PE name, start and end points, will be updated during draw Process
         self.platformDrawn = False
+        self.mappingDrawn = False
         self.PESize = height                        #the minimum of PE length or height after drawing, is set to height because no PE can be drawn with initial height value
-                                                    #so it must reduce 
-    
-    '''
-    method is used internally to determine the colors for the drawn elements
-    '''    
-    def resolveColor(self, colorValue):
-        if isinstance(colorValue, int) and colorValue <= 20 and colorValue > 0:
-            return self.colorDict[colorValue]
-        else:
-            raise ValueError('Wrong color value to resolve given!')
+                                                    #so it must reduce
+        self.mappingIDs = []
+        self.mappingHandles = []                    #list contains handles of all drawn mapping elements in purpose to easily remove them
         
+        self.initializePlatformColors(platformColors)
+        self.initializeMappingColors(mappingColors)
+          
     '''
     first method that should be called, requires an object of the platform class and sets platform description and equalList
     attributes of drawAPI instance
@@ -80,9 +59,51 @@ class drawAPI():
     requires an object of the mapping class and sets the mapping description of the drawAPI instance, should be called before
     attempting to draw the mapping
     '''
-    def addMapping(self, mapping):
-        self.mappingDescription = mapping.to_coreList()
+    def addMapping(self, mapping, mappingID):
+        if listOperations.containsItem(listOperations, self.mappingIDs, mappingID) or not isinstance(mappingID, int):
+            raise(RuntimeError('This mapping id is already in use or not an integer!'))
+        else:
+            self.mappingIDs.append(mappingID)
+            
+        tmpMappingDescription = mapping.to_coreList()
+        
+        if self.mappingDescription == {}:
+            for key in tmpMappingDescription:
+                self.mappingDescription.update({key : {}})
+        
+        for key in tmpMappingDescription:
+            if len(tmpMappingDescription[key]) == 0:
+                continue
+            else:
+                self.mappingDescription[key].update({mappingID : tmpMappingDescription[key]})
+        if self.mappingDrawn:
+            self.clearMappings()
+            self.drawMapping()
+        
         return
+    
+    '''
+    removes the mapping belonging to the given mapping ID and redraw the mappings
+    '''
+    def removeMapping(self, mappingID):
+        if not listOperations.containsItem(listOperations, self.mappingIDs, mappingID):
+            raise(RuntimeError('This ID is currently not in use!'))
+        else:
+            for key in self.mappingDescription:
+                if mappingID in self.mappingDescription[key]:
+                    del self.mappingDescription[key][mappingID]
+            self.mappingIDs.remove(mappingID)
+            if self.mappingDrawn:
+                self.clearMappings()
+        return    
+    '''
+    deletes all drawn mapping elements
+    '''
+    def clearMappings(self):  
+        for handle in self.mappingHandles:
+            self.drawDevice.delete(handle)
+        self.mappingHandles = []
+        self.drawMapping()
     
     '''
     after initialization of the drawAPI class via constructor and setPlatform method, draw can be called which introduces
@@ -96,17 +117,17 @@ class drawAPI():
         
         i = 0
         for item in toDraw:
-            self.drawInnerRessource(item, drawWidth, drawHeight, i, 0, 20, len(toDraw))
+            self.drawInnerRessource(item, drawWidth, drawHeight, i, 0, 1, len(toDraw))
             i += 1
 
         return
     
     '''
-    NOT COMPLETLY IMPLEMENTED YET
     checks if a platform has been drawn and a mapping description is set, if so it draws the mapping description in top
     of the platform description
     '''
     def drawMapping(self):
+        self.mappingDrawn = True
         if self.mappingDescription == {}:
             raise(RuntimeError('Please set a mapping description before attempt to draw a mapping'))
         if not self.platformDrawn:
@@ -115,35 +136,63 @@ class drawAPI():
         keyWithMostProcs = None
         mostProcs = 0
         for key in self.mappingDescription:
-            if len(self.mappingDescription[key]) > mostProcs:
-                mostProcs = len(self.mappingDescription[key])
+            amount = 0
+            for innerKey in self.mappingDescription[key]:
+                amount += len(self.mappingDescription[key][innerKey])
+            if amount > mostProcs:
+                mostProcs = amount
                 keyWithMostProcs = key
-                
-        matrix = listOperations.convertToMatrix(listOperations, self.mappingDescription[keyWithMostProcs])
+        
+        #creating a tempList containing all mapping elements of the PE with most mapping elements on it, its not really needed
+        #and just for the purpose of calculating the draw size of the mapping dots
+        tmpList = []
+        for key in self.mappingDescription[keyWithMostProcs]:
+                for element in self.mappingDescription[keyWithMostProcs][key]:
+                    tmpList.append(element)
+        matrix = listOperations.convertToMatrix(listOperations, tmpList)
         maxLength = listOperations.getDimension(listOperations, matrix)
         radius = self.PESize / (2 * maxLength)
         
         for key in self.mappingDescription:
-            if len(self.mappingDescription[key]) > 0:
+            if bool(self.mappingDescription[key]):
                 startPointX = self.coreDict[key][0]
                 endPointX = self.coreDict[key][1]
                 startPointY = self.coreDict[key][2]
                 currentX = startPointX + radius
                 currentY = startPointY + radius
-                
-                for entry in self.mappingDescription[key]:
-                    if (currentX + radius) > endPointX:
-                        currentY += 2 * radius
-                        currentX = startPointX + radius
-                    self.drawDevice.create_oval(currentX - radius, currentY - radius, currentX + radius, currentY + radius, fill = self.resolveColor(1))
-                    self.drawDevice.create_text(currentX, currentY, text = entry)
-                    currentX += (2 * radius)
+                for innerKey in self.mappingDescription[key]:
+                    color = self.resolveMappingColor(innerKey)
+                    for entry in self.mappingDescription[key][innerKey]:
+                        if (currentX + radius) > endPointX:
+                            currentY += 2 * radius
+                            currentX = startPointX + radius
+                        self.mappingHandles.append(self.drawDevice.create_oval(currentX - radius, currentY - radius, currentX + radius, currentY + radius, fill = color))
+                        self.mappingHandles.append(self.drawDevice.create_text(currentX, currentY, text = entry))
+                        currentX += (2 * radius)
             
             else:
                 continue
         
         
         return
+    
+    '''
+    method is used internally to determine the colors for the drawn elements, should not be called manually
+    '''    
+    def resolvePlatformColor(self, colorValue):
+        if isinstance(colorValue, int) and colorValue <= 10 and colorValue > 0:
+            return self.platformColors[colorValue]
+        else:
+            raise ValueError('Wrong color value to resolve given!')
+        
+    '''
+    method is used internally to determine the colors for the drawn elements, should not be called manually
+    '''    
+    def resolveMappingColor(self, colorValue):
+        if isinstance(colorValue, int) and colorValue <= 10 and colorValue > 0:
+            return self.mappingColors[colorValue]
+        else:
+            raise ValueError('Wrong color value to resolve given!')
         
     '''
     method draws primitives, should not be called manually
@@ -162,7 +211,7 @@ class drawAPI():
         textPointX = endPointX - (endPointX - startPointX) / 2
         textPointY = endPointY - (endPointY  - startPointY) / 2
         
-        color = self.resolveColor(colorValue)
+        color = self.resolvePlatformColor(colorValue)
         
         self.drawDevice.create_rectangle(startPointX, startPointY, endPointX, endPointY, fill = color)
         self.drawDevice.create_text(textPointX, textPointY, text = toDraw[0])
@@ -182,7 +231,7 @@ class drawAPI():
             if not isinstance(item, tuple) or len(item[1]) > 1:
                 drawPEsWithPrimitive = False
         
-        nextColor = colorValue - 1 
+        nextColor = colorValue + 1 
         
         if drawPEs or drawPEsWithPrimitive:
             if drawWidth < restSizeY:
@@ -225,8 +274,8 @@ class drawAPI():
         elif sizeX <= sizePEY and sizeX < self.PESize:
             self.PESize = sizeX
         
-        colorPE = self.resolveColor(colorValue)
-        colorPrimitive = self.resolveColor(colorValue - 1)
+        colorPE = self.resolvePlatformColor(colorValue)
+        colorPrimitive = self.resolvePlatformColor(colorValue + 1)
         
         i = 0
         for row in matrix:
@@ -277,7 +326,7 @@ class drawAPI():
         elif sizeX <= sizeY and sizeX < self.PESize:
             self.PESize = sizeX
     
-        color = self.resolveColor(colorValue)
+        color = self.resolvePlatformColor(colorValue)
     
         i = 0
         for row in matrix:
@@ -313,7 +362,50 @@ class drawAPI():
                 j += 1
             i += 1
         return    
-        
+    
+    '''
+    method initialize the color dictionary for platform elements, should not be called manually
+    '''
+    def initializePlatformColors(self, colorList):
+        if colorList[0] == 'default':
+            self.platformColors = { 1: 'orange',
+                                    2: 'chartreuse1',
+                                    3: 'azure1',
+                                    4: 'cornsilk2',
+                                    5: 'coral',
+                                    6: 'cornflowerblue',
+                                    7: 'cyan2',
+                                    8: 'darkgoldenrod1',
+                                    9: 'aqua',
+                                    10: 'darkorchid'}
+        else:
+            i = 1
+            for entry in colorList:
+                self.platformColors.update({i : entry})
+                i += 1
+        return
+    
+    '''
+    method initialize the color dictionary for platform elements, should not be called manually
+    '''
+    def initializeMappingColors(self, colorList):
+        if colorList[0] == 'default':
+            self.mappingColors = { 1:'aliceblue', 
+                                   2:'darkorange', 
+                                   3:'chocolate', 
+                                   4:'yellow', 
+                                   5:'bisque1', 
+                                   6:'blue', 
+                                   7:'blueviolet', 
+                                   8:'brown1', 
+                                   9:'burlywood',
+                                   10: 'cadetblue'}
+        else:
+            i = 1
+            for entry in colorList:
+                self.mappingColors.update({i : entry})
+                i += 1
+        return
         
         
         
