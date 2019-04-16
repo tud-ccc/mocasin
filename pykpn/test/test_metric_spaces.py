7b120a95bc77db1dd76c38549a4f9bae7402fcc6
Felix Teweleit <felix.teweleit@yahoo.de>
2019-04-16 10:03:08 +0200

    Finished unit test

--- pykpn/test/test_metric_spaces.py
+++ pykpn/test/test_metric_spaces.py
@@ -8,7 +8,7 @@ import unittest
 from pykpn.representations.metric_spaces import *
 from pykpn.representations.examples import *
 
-class Metric_Spaces_Test(unittest.TestCase):
+class MetricSpaces_Test(unittest.TestCase):
     
     def setUp(self):
         self.N = 10000
@@ -23,7 +23,7 @@ class Metric_Spaces_Test(unittest.TestCase):
                                 {'N0': 0, 'N2': 1, 'N3': 2, 'N1': 3, 'N4': 4, 'N5': 5}),
                               'Error performing dijkstra')
 
-    def test_probabilities(self):
+    def test_finiteMetricSpace_uniformFromBall(self):
         testSpace = exampleClusterArch
         runs = testSpace.uniformFromBall(3,1,self.N)
         result = list(zip(range(4),map(lambda x : len(x)/float(self.N), [ [run for run in runs if run == i] for i in range(4)])))
@@ -31,16 +31,25 @@ class Metric_Spaces_Test(unittest.TestCase):
         for probabilitie in result:
             self.assertTrue((probabilitie[1] == 0.0 or (probabilitie[1] >= 0.3 and probabilitie[1] <= 0.35)), 'Error calculating probabilities')
         
-    def test_uniform_in_ball_length(self):
-        testSpace = exampleClusterArch
-        testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
-        result = len(testProdSpace.ball([3,2,7],4))
-        self.assertEqual(result, 1072, "Error in ball()")
-    
-    def test_uniform_in_ball_list(self):
+    def test_finiteMetricSpaceLP_ball1(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
         
+        result = len(testProdSpace.ball([3,2,7],4))
+        self.assertEqual(result, 1072, "Error in ball()")
+        
+    def test_finiteMetricSpaceLP_ball2(self):
+        testSpace = exampleClusterArch
+        testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
+        oneBall = testProdSpace.ball([3,2,7],1)
+        
+        result = list(map(testProdSpace.int2Tuple, oneBall ))
+        self.assertListEqual(result, [[3, 2, 4], [3, 2, 5], [3, 2, 6], [3, 0, 7], [3, 1, 7], [0, 2, 7], [1, 2, 7], [2, 2, 7], [3, 2, 7], [3, 3, 7]],
+                            'Error in ball()')
+    
+    def test_finiteMetricSpaceLP_uniformFromBall1(self):
+        testSpace = exampleClusterArch
+        testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
         uniformFromFourBall = testProdSpace.uniformFromBall([3,2,7],4,10)
         
         self.assertTrue(len(uniformFromFourBall) == 10, 'Error in uniformFromBall(). To many output values!')
@@ -49,7 +58,7 @@ class Metric_Spaces_Test(unittest.TestCase):
             self.assertTrue(testProdSpace.dist(testProdSpace.tuple2Int([3,2,7]), value) <= 4.0,
                             'Error in uniformFromBall(). Points out of ball!')
         
-    def test_inverses(self):
+    def test_finiteMetricSpaceLP_uniformFromBall2(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
         uniformFromFourBall = testProdSpace.uniformFromBall([3,2,7],4,10)
@@ -58,13 +67,14 @@ class Metric_Spaces_Test(unittest.TestCase):
         result = list(map(testProdSpace.tuple2Int, result))
         self.assertListEqual(result, uniformFromFourBall,'Error in int2Tuple()')
         
-    def test_dist_calc(self):
+    def test_finiteMetricSpaceLP_calc(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
+        
         result = testProdSpace._distCalc([3,2,7],[3,0,4])
         self.assertEqual(result, 2.0, 'Error in _distCalc()')
         
-    def test_dist(self):
+    def test_finiteMetricSpaceLP_dist(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
         uniformFromFourBall = testProdSpace.uniformFromBall([3,2,7],4,10)
@@ -78,24 +88,26 @@ class Metric_Spaces_Test(unittest.TestCase):
         for value in result:
             self.assertTrue((value <= 4.0), 'Error in dist(). Value outside of the ball!')
     
-    def test_ball_as_tuple(self):
+    def test_finiteMetricSpaceLP_uniform(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
-        oneBall = testProdSpace.ball([3,2,7],1)
-        result = list(map(testProdSpace.int2Tuple, oneBall ))
-        self.assertListEqual(result, [[3, 2, 4], [3, 2, 5], [3, 2, 6], [3, 0, 7], [3, 1, 7], [0, 2, 7], [1, 2, 7], [2, 2, 7], [3, 2, 7], [3, 3, 7]],
-                            'Error in ball()')
+        
+        result = testProdSpace.uniform()
+        for value in result:
+            self.assertTrue((testProdSpace.dist([3,2,7], testProdSpace.int2Tuple(value)) <= 5,
+                             'Error in uniform()'))
     
-    def test_one_ball_length(self):
+    def test_finiteMetricSpaceLP_oneBall(self):
         testSpace = exampleClusterArch
         testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
         oneBall = testProdSpace.ball([3,2,7],1)
+        
         self.assertEqual(len(oneBall), 10, 'Error in ball()')
         
-    def test_sym_space_length(self):
+    def test_FiniteMetricSpaceLPSym_length(self):
         result = FiniteMetricSpaceLPSym(exampleClusterArchSymmetries,d=2).n
         self.assertEqual(result, 20, 'Error in calculating sym_space length')
-    
+        
     def test_dist_1(self):
         result = exampleClusterArchSymmetries.dist([3],[4])
         self.assertEqual(result, 2, 'Error in dist() with ClusterArchsymmetries')
@@ -127,28 +139,5 @@ class Metric_Spaces_Test(unittest.TestCase):
         self.assertEqual(result, frozenset({(3, 4, 3), (0, 7, 0), (2, 6, 2), (1, 7, 1), (2, 7, 2), (1, 4, 1), (2, 4, 2), (0, 4, 0), 
                                             (1, 6, 1), (3, 5, 3), (3, 6, 3), (1, 5, 1), (0, 5, 0), (2, 5, 2), (3, 7, 3), (0, 6, 0)}),
                                             "Error in tuple_orbit()")
-    
-"""
-print("using dijkstra everywhere:" + str(arch_graph_to_distance_metric(exampleDiijkstra)))
-N = 10000
-testSpace = exampleClusterArch
-runs = testSpace.uniformFromBall(3,1,N)
-print("probabilities for " + str(N) +" runs:  " + str(list(zip(range(4),map(lambda x : len(x)/float(N), [ [run for run in runs if run == i] for i in range(4)])))))
-
-testProdSpace = FiniteMetricSpaceLP(testSpace,d=3)
-uniformFromFourBall = testProdSpace.uniformFromBall([3,2,7],4,10)
-print("uniform in ball [3,2,7], 4  (" + str(len(testProdSpace.ball([3,2,7],4))) +" elements): " + str(uniformFromFourBall) + " = " + str(list(map(testProdSpace.int2Tuple,uniformFromFourBall))))
-print("dist([3,2,7],[3,0,4] = " + str(testProdSpace._distCalc([3,2,7],[3,0,4])))
-print("; ".join( [ "dist( [3,2,7]," + str(testProdSpace.int2Tuple(j)) + ") = " + str(testProdSpace.dist(testProdSpace.tuple2Int([3,2,7]),j))  for j in uniformFromFourBall]))
-oneBall = testProdSpace.ball([3,2,7],1)
-print("the ball [3,2,7] , 1 (as tuples) " + str(list(map( testProdSpace.int2Tuple, oneBall ))))
-assert(len(oneBall)==10)
-
-testSymSpace = FiniteMetricSpaceLPSym(exampleClusterArchSymmetries,d=3)
-print("sym_space with d=2 has length: " + str(FiniteMetricSpaceLPSym(exampleClusterArchSymmetries,d=2).n))
-print("dist_sym(3,4) = " + str(exampleClusterArchSymmetries.dist([3],[4]))) # should be 2, 0 if we have wreath
-print("dist_sym(3,0) = " + str(exampleClusterArchSymmetries.dist([3],[0]))) # should be 0
-print("dist_sym([3,2,7],[3,0,4]) = " + str(testSymSpace.dist([3,2,7],[3,0,4]))) # should be 0
-print("dist_sym([3,4,7],[3,0,4]) = " + str(testSymSpace.dist([3,4,7],[3,0,4]))) # should be 2
-print("dist_sym([3,4,3],[5,11,4]) = " + str(testSymSpace.dist([3,4,3],[5,11,4]))) #should be 6, 1 if we have wreath
-"""
+if __name__ == "__main__":
+    unittest.main()
