c21238ce7b99a7a1f4de8c88c60c867fb8234aff
Christian Menard <christian.menard@tu-dresden.de>
2019-06-03 15:42:03 +0200

    move maps scrip mapping_to_dot to scripts/maps

--- scripts/slx/mapping_to_dot.py
+++ scripts/slx/mapping_to_dot.py
@@ -7,6 +7,7 @@
 
 
 import argparse
+import sys
 
 from pykpn.util import logging
 from pykpn.slx.kpn import SlxKpnGraph
@@ -17,7 +18,7 @@ from pykpn.slx.platform import SlxPlatform
 log = logging.getLogger(__name__)
 
 
-def main():
+def main(argv):
     parser = argparse.ArgumentParser()
 
     logging.add_cli_args(parser)
@@ -32,7 +33,7 @@ def main():
     parser.add_argument('--slx-version', help="dot output file", type=str,
                         default='2017.04')
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     logging.setup_from_args(args)
 
@@ -44,4 +45,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
