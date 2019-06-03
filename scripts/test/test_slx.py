ae698363c31ffaf0b902ee946bc235a76f72edab
Christian Menard <christian.menard@tu-dresden.de>
2019-06-03 15:35:53 +0200

    move test for simulate script to test_maps and add all required input files

--- scripts/test/test_slx.py
+++ scripts/test/test_slx.py
@@ -4,9 +4,12 @@
 # Authors: Christian Menard
 
 import filecmp
+import os
 import pytest
+
 from scripts.slx.platform_to_dot import main as platform_to_dot
 from scripts.slx.kpn_to_dot import main as kpn_to_dot
+from scripts.slx.simulate import main as simulate
 
 
 slx_versions = ["2017.04", "2017.10"]
@@ -29,3 +32,17 @@ def test_kpn_to_dot(datadir, version):
     expected = datadir.join("audio_filter.dot.expected")
     kpn_to_dot([str(xml), str(dot), "--slx-version=%s" % version])
     assert filecmp.cmp(dot, expected)
+
+
+# XXX This Test can currently not be run for parallella as it imports another
+#     version of the SLX bindings then exynos and multidsp. As this leads to a
+#     conflict the test would alway fail...
+@pytest.mark.parametrize("platform, expected", [
+    ("exynos", "19.841971309 ms"),
+    ("multidsp", "95.811217897 ms"),
+])
+def test_simulate(capfd, datadir, platform, expected):
+    os.chdir(datadir)
+    simulate([str("%s.ini" % platform)])
+    out, err = capfd.readouterr()
+    assert out.split('\n')[-3] == "Total simulated time: %s" % expected
