2be720734acf7e89e7b753d1110f203596878b9c
Christian Menard <christian.menard@tu-dresden.de>
2020-02-26 09:57:44 +0100

    read search paths from the environment variable PYKPN_CONF_PATH

--- hydra_plugins/pykpn_searchpath_plugin/pykpn_searchpath_plugin.py
+++ hydra_plugins/pykpn_searchpath_plugin/pykpn_searchpath_plugin.py
@@ -3,10 +3,34 @@
 #
 # Authors: Christian Menard
 
+import os
+
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
 
 class PykpnSearchPathPlugin(SearchPathPlugin):
+    """
+    A hydra plugin that extends the search path that hydra uses to find
+    configurations.
+
+    The plugin adds the directory 'conf/' relative to the CWD which allows
+    users to define project specific configuration. In case multiple
+    configuration directories are required or users want to point to other
+    directories, the user can set the environment variable ~PYKPN_CONF_PATH~.
+    The plugin expects a ':' separated list of paths which it appends to the
+    search path.
+    """
+
     def manipulate_search_path(self, search_path):
+        # append paths from the environment variable PYKPN_CONF_PATH
+        pykpn_conf_path = os.environ.get('PYKPN_CONF_PATH')
+        print(pykpn_conf_path)
+        if pykpn_conf_path is not None:
+            split = pykpn_conf_path.split(":")
+            for path in split:
+                search_path.append(
+                    provider="pykpn-searchpath-plugin", path=path)
+
+        # append conf directory located in CWD
         search_path.append(
             provider="pykpn-searchpath-plugin", path="conf/")
