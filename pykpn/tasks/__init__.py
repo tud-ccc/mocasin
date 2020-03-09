be6bff167fba555f4592341dd554a08a8bfa5404
Christian Menard <christian.menard@tu-dresden.de>
2020-03-09 09:47:19 +0100

    introduce a help task

--- pykpn/tasks/__init__.py
+++ pykpn/tasks/__init__.py
@@ -14,6 +14,7 @@ expect precisely one parameter, the Omniconf object as created by hydra.
 import hydra
 import logging
 import sys
+import textwrap
 
 from pykpn.tasks.csv_plot import csv_plot
 from pykpn.tasks.design_centering import dc_task
@@ -30,20 +31,66 @@ from pykpn.tgff.tgffSimulation import TgffReferenceError
 
 log = logging.getLogger(__name__)
 
-pykpn_tasks = {
-    'csv_plot': csv_plot,
-    'design_centering': dc_task,
-    'enumerate_equivalent': enumerate_equivalent,
-    'kpn_to_dot': kpn_to_dot,
-    'mapping_to_dot': mapping_to_dot,
-    'platform_to_autgrp': platform_to_autgrp,
-    'platform_to_dot': platform_to_dot,
-    'random_walk_mapping': random_walk,
-    'simulate': simulate,
-    'unit_tests' : unit_tests,
-    'visualize' : visualize,    
+
+def print_help(cfg=None):
+    _print_help_impl()
+
+
+_tasks = {
+    'csv_plot': (
+        csv_plot,
+        "???"),
+    'design_centering': (
+        dc_task,
+        "generate a mapping using the design centering algorityh"),
+    'enumerate_equivalent': (
+        enumerate_equivalent,
+        "???"),
+    'help': (
+        print_help,
+        "Print a help message"),
+    'kpn_to_dot': (
+        kpn_to_dot,
+        "Visualize a KPN application as a dot graph"),
+    'mapping_to_dot': (
+        mapping_to_dot,
+        "Visualize a mapping as a dot graph"),
+    'platform_to_autgrp': (
+        platform_to_autgrp,
+        "Calculate the Automorphism Group of a Platform Graph"),
+    'platform_to_dot': (
+        platform_to_dot,
+        "Visualize a platform as a dot graph"),
+    'random_walk_mapping': (
+        random_walk,
+        "Generate a mapping using the random walk algorithm"),
+    'simulate': (
+        simulate,
+        "Replay traces to simulate the execution of a KPN application on a "
+        "given platform"),
+    'visualize': (
+        visualize,
+        "Visualize a mapping in the GUI"),
 }
 """A dictionary that maps task names to a callable function."""
+
+
+def _print_help_impl():
+    print("pykpn is a framework for modeling KPN applications and their")
+    print("execution on MPSoC platforms.")
+    print("")
+    print("Usage: pykpn TASK [HYDRA OPTIONS]")
+    print("")
+    print("pykpn can perform one of several tasks. It expects the first ")
+    print("argument to specify the task to be executed. Choose one of:")
+    print("")
+    for kv in _tasks.items():
+        desc = kv[1][1]
+        desc_lines = textwrap.wrap(desc, width=41)
+        task = "  %s: " % kv[0]
+        print("%s%s" % ("{:<24}".format(task), desc_lines[0]))
+        for line in desc_lines[1:]:
+            print("%s%s" % ("{:<24}".format(''), line))
 
 
 @hydra.main(config_path='conf/default.yaml')
@@ -54,19 +101,19 @@ def execute_task(cfg):
     :return:
     """
     task = cfg['task']
-    
-    if task not in pykpn_tasks:
+
+    if task not in _tasks:
         log.error("Tried to run a task unknown to pykpn (%s)" % task)
-        log.info("Choose one of %s" % list(pykpn_tasks.keys()))
+        print(help)
+        # log.info("Choose one of %s" % list(pykpn_tasks.keys()))
         sys.exit(-1)
 
     try:
         # execute the task
-        function = pykpn_tasks[task]
+        function = _tasks[task][0]
         function(cfg)
     except TgffReferenceError:
-        #Special exception indicates a bad combination of tgff components
-        #can be thrown during multiruns and should not stop the hydra
-        #execution 
+        # Special exception indicates a bad combination of tgff components
+        # can be thrown during multiruns and should not stop the hydra
+        # execution
         log.warning("Referenced non existing tgff component!")
-
