65425ba7be11a8726f08a0d1f521e84b7d95273b
teweleit <felix.teweleit@yahoo.de>
2020-04-07 11:41:09 +0200

    Refactored statistics for mapping generation. Added statistics to gbm and random_walk

---
+++ pykpn/mapper/utils.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2019 TU Dresden
+# All Rights Reserved
+#
+# Authors: Andrès Goens, Felix Teweleit
+
+from pykpn.util.logging import getLogger
+
+class Statistics(object):
+
+    def __init__(self, logger, process_amount):
+        self._log = logger
+        self._processes = process_amount
+        self._mappings_cached = 0
+        self._mappings_evaluated = 0
+        self._simulation_time = 0
+        self._representation_time = 0
+
+    def mapping_cached(self):
+        self._mappings_cached += 1
+
+    def mapping_evaluated(self, simulation_time):
+        self._mappings_evaluated += 1
+        self._simulation_time += simulation_time
+
+    def add_offset(self, time):
+        self._simulation_time += time
+
+    def log_statistics(self):
+        self._log.info(f"Amount of processes in task:  {self._processes}")
+        self._log.info(f"Mappings cached: {self._mappings_cached}")
+        self._log.info(f"Mappings evaluated: {self._mappings_evaluated}")
+        self._log.info(f"Time spend simulating: {self._simulation_time}")
+
+    def to_file(self):
+        file = open('statistics.txt', 'x')
+        file.write("Processes: " + str(self._processes) + "\n")
+        file.write("Mappings cached: " + str(self._mappings_cached) + "\n")
+        file.write("Mappings evaluated: " + str(self._mappings_evaluated) + "\n")
+        file.write("Time spend simulating: " + str(self._simulation_time) + "\n")
+        file.write("Representation time: " + str(self._representation_time) + "\n")
+        file.close()
+
+
+class MappingCache(object):
+
+    def __init__(self):
+        self._cache = {}
+        self._last_added = None
+
+    def lookup(self, mapping):
+        if mapping not in self._cache:
+            self._cache.update({mapping : None})
+            self._last_added = mapping
+            return False
+
+        return self._cache[mapping]
+
+    def add_time(self, time):
+        if not self._last_added:
+            raise RuntimeError("Cache mapping before adding a simulation time!")
+
+        self._cache[self._last_added] = time
+        self._last_added = None
