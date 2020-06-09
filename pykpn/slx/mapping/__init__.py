8b0db057f0f37579f94790a132fbc98026db52df
Christian Menard <christian.menard@tu-dresden.de>
2020-06-09 09:47:17 +0200

    drop support for multiple maps versions when parsing mappings

--- pykpn/slx/mapping/__init__.py
+++ pykpn/slx/mapping/__init__.py
@@ -9,6 +9,7 @@ import pint
 from pykpn.util import logging
 from pykpn.common.mapping import (ChannelMappingInfo, Mapping,
     ProcessMappingInfo, SchedulerMappingInfo)
+from pykpn.slx.mapping import slxmapping
 
 
 log = logging.getLogger(__name__)
@@ -23,8 +24,8 @@ class SlxMapper:
     calling the generate_mapping method.  This implements the common mapper
     interface.
     """
-    def __init__(self, kpn, platform, cfg, mapping_xml=None, slx_version=None):
-        self.mapping = SlxMapping(kpn, platform, mapping_xml, slx_version)
+    def __init__(self, kpn, platform, cfg, mapping_xml=None):
+        self.mapping = SlxMapping(kpn, platform, mapping_xml)
 
     def generate_mapping(self):
         return self.mapping
@@ -32,23 +33,14 @@ class SlxMapper:
 
 class SlxMapping(Mapping):
 
-    #config parameter is not needed but still added to remain the interface of other mappings types in order
-    #to ensure instantiation via hydra
-    def __init__(self, kpn, platform, mapping_xml=None, slx_version=None):
+    def __init__(self, kpn, platform, mapping_xml):
         super().__init__(kpn, platform)
 
         log.info('Start parsing the SLX mapping ' + mapping_xml)
 
         # load the xml
         with open(mapping_xml) as mf:
-            if (slx_version == '2017.10'):
-                from ._2017_10 import slxmapping
-                xml_mapping = slxmapping.CreateFromDocument(mf.read())
-            elif (slx_version == '2017.04'):
-                from ._2017_04 import slxmapping
-                xml_mapping = slxmapping.CreateFromDocument(mf.read())
-            else:
-                raise ValueError('SLX version %s is not supported!' % slx_version)
+            xml_mapping = slxmapping.CreateFromDocument(mf.read())
 
         # keep track of the mapping process->scheduler
         process_scheduler = {}
@@ -57,24 +49,14 @@ class SlxMapping(Mapping):
             name = xs.id
             scheduler = platform.find_scheduler(name)
 
+            # TODO delete policy selection
             # the policy mechanism differs depending on the version
-            if (slx_version == '2017.10'):
-                policy = scheduler.policies[0]
-                param = None
-                log.warning('2017.10 mapping descriptors do not specify the '
-                            'scheduling policy. -> Set the policy for %s to the '
-                            'first policy specified by the platform (%s)' %
-                            (name, policy.name))
-            elif (slx_version == '2017.04'):
-                if xs.timeSliceValue is not None:
-                    time_slice = _ur(str(xs.timeSliceValue) + xs.timeSliceUnit)
-                    param = time_slice.to('ps').magnitude
-                else:
-                    param = None
-                policy = scheduler.find_policy(xs.policy)
-            else:
-                raise NotImplementedError(
-                    'policy parsing not implemented for version %s!' % version)
+            policy = scheduler.policies[0]
+            param = None
+            log.warning('2017.10 mapping descriptors do not specify the '
+                        'scheduling policy. -> Set the policy for %s to the '
+                        'first policy specified by the platform (%s)' %
+                        (name, policy.name))
 
             for pref in xs.ProcessRef:
                 pname = pref.process
@@ -84,7 +66,6 @@ class SlxMapping(Mapping):
             log.debug('configure scheduler %s to use policy %s' %
                       (name, policy.name))
 
-        # parse processes
         for xp in xml_mapping.Process:
             name = xp.id
             p_name = xp.ProcessorAffinityRef[0].processor
@@ -108,18 +89,11 @@ class SlxMapping(Mapping):
 
         log.info('Done parsing the SLX mapping')
 
-    def export(self, file_name, version):
-        export_slx_mapping(self, file_name, version)
+    def export(self, file_name):
+        export_slx_mapping(self, file_name)
 
 
-def export_slx_mapping(mapping, file_name, version):
-    if (version == '2017.10'):
-        from ._2017_10 import slxmapping
-    elif (version == '2017.04'):
-        from ._2017_04 import slxmapping
-    else:
-        raise ValueError('SLX version %s is not supported!' % version)
-
+def export_slx_mapping(mapping, file_name):
     xml_mapping = slxmapping.MappingType()
     xml_mapping.version = '1.0'
     xml_mapping.platformName = mapping.kpn.name
@@ -188,21 +162,6 @@ def export_slx_mapping(mapping, file_name, version):
             xml_process_ref = slxmapping.ProcessRefType()
             xml_process_ref.process = p.name
             xml_scheduler.ProcessRef.append(xml_process_ref)
-
-        if version == '2017.10':
-            log.warning('slx 2017.10 mapping descriptors do not support '
-                        'scheduling policies -> truncate while exporting')
-        elif version == '2017.04':
-            xml_scheduler.policy = info.policy.name
-            # has param?
-            if info.param is not None:
-                if isinstance(info.param, int):
-                    # interpret it as the time slice value
-                    xml_scheduler.timeSliceValue = info.param
-                    xml_scheduler.timeSliceUnit = 'ps'
-                else:
-                    log.warning('found a unknown scheduler parameter '
-                                '-> ignore it during export')
 
         xml_mapping.Scheduler.append(xml_scheduler)
 
