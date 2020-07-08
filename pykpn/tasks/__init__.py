e9389b08b6c3acdd38b2e33368b15ff7b27b1d73
Christian Menard <christian.menard@tu-dresden.de>
2020-07-08 17:35:27 +0200

    import tasks on demand
    This avoids always importing all the tasks and significantly speeds up
    execution time

--- pykpn/tasks/__init__.py
+++ pykpn/tasks/__init__.py
@@ -16,18 +16,8 @@ import logging
 import sys
 import textwrap
 
-from pykpn.tasks.csv_plot import csv_plot
-from pykpn.tasks.design_centering import dc_task
-from pykpn.tasks.enumerate_equivalent import enumerate_equivalent
-from pykpn.tasks.platform_to_autgrp import platform_to_autgrp
-from pykpn.tasks.generate_mapping import generate_mapping
-from pykpn.tasks.simulate import simulate
-from pykpn.tasks.to_dot import kpn_to_dot
-from pykpn.tasks.to_dot import mapping_to_dot
-from pykpn.tasks.to_dot import platform_to_dot
-from pykpn.tasks.visualize import visualize
-from pykpn.tasks.solve_query import solve_query
-from pykpn.tasks.generate_yaml import generate_yaml
+from importlib import import_module
+
 
 from pykpn.tgff.tgffSimulation import TgffReferenceError
 
@@ -40,44 +30,57 @@ def print_help(cfg=None):
 
 _tasks = {
     'csv_plot': (
-        csv_plot,
+        'csv_plot',
+        'csv_plot',
         "???"),
     'design_centering': (
-        dc_task,
+        'design_centering',
+        'dc_task',
         "generate a mapping using the design centering algorityh"),
     'enumerate_equivalent': (
-        enumerate_equivalent,
+        'enumerate_equivalent',
+        'enumerate_equivalent',
         "ennumerate all mappings equivalent to the given mapping"),
     'help': (
-        print_help,
+        None,
+        None,
         "Print a help message"),
     'kpn_to_dot': (
-        kpn_to_dot,
+        'to_dot',
+        'kpn_to_dot',
         "Visualize a KPN application as a dot graph"),
     'mapping_to_dot': (
-        mapping_to_dot,
+        'to_dot',
+        'mapping_to_dot',
         "Visualize a mapping as a dot graph"),
     'platform_to_autgrp': (
-        platform_to_autgrp,
+        'platform_to_autgrp',
+        'platform_to_autgrp',
         "Calculate the Automorphism Group of a Platform Graph"),
     'platform_to_dot': (
-        platform_to_dot,
+        'to_dot',
+        'platform_to_dot',
         "Visualize a platform as a dot graph"),
     'generate_mapping': (
-        generate_mapping,
+        'generate_mapping',
+        'generate_mapping',
         "Generate a mapping."),
     'simulate': (
-        simulate,
+        'simulate',
+        'simulate',
         "Replay traces to simulate the execution of a KPN application on a "
         "given platform"),
     'visualize': (
-        visualize,
+        'visualize',
+        'visualize',
         "Visualize a mapping in the GUI"),
     'solve_query' : (
-        solve_query,
+        'solve_query',
+        'solve_query',
         "Generates a mapping based on constraints expressed in a query language"),
     'generate_yaml': (
-        generate_yaml,
+        'generate_yaml',
+        'generate_yaml',
         "Generates a bunch of yaml files"),
 }
 """A dictionary that maps task names to a callable function."""
@@ -93,7 +96,7 @@ def _print_help_impl():
     print("argument to specify the task to be executed. Choose one of:")
     print("")
     for kv in _tasks.items():
-        desc = kv[1][1]
+        desc = kv[1][2]
         desc_lines = textwrap.wrap(desc, width=41)
         task = "  %s: " % kv[0]
         print("%s%s" % ("{:<24}".format(task), desc_lines[0]))
@@ -118,12 +121,19 @@ def execute_task(task):
         print_help()
         sys.exit(-1)
 
-    try:
-        # execute the task
-        function = _tasks[task][0]
-        function()
-    except TgffReferenceError:
-        # Special exception indicates a bad combination of tgff components
-        # can be thrown during multiruns and should not stop the hydra
-        # execution
-        log.warning("Referenced non existing tgff component!")
+    if task == 'help':
+        print_help()
+    else:
+        try:
+            # load the task
+            module_name = _tasks[task][0]
+            function_name = _tasks[task][1]
+            module = import_module(f"pykpn.tasks.{module_name}")
+            function = getattr(module, function_name)
+            # execute the task
+            function()
+        except TgffReferenceError:
+            # Special exception indicates a bad combination of tgff components
+            # can be thrown during multiruns and should not stop the hydra
+            # execution
+            log.warning("Referenced non existing tgff component!")
