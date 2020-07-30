6e8e203ce2cdbbad14b05d493150cf804b1f99bf
Robert Khasanov <robert.khasanov@tu-dresden.de>
2020-07-30 11:40:49 +0200

    Added test for dac-2 schedueler.
    Tests were rewritten from lit-testing format into pytest

--- test/test_tetris.py
+++ test/test_tetris.py
@@ -22,18 +22,26 @@ def tetris_scheduler_test(request):
     return request.param
 
 
-def test_tetris_dac(datadir, expected_dir, tetris_scheduler_test):
-    testname = os.path.splitext(tetris_scheduler_test)[0]
+def run_tetris(datadir, expected_dir, scheduler, scenario, mode):
+    testname = os.path.splitext(scenario)[0]
+    sched_l = scheduler.lower()
     input_scn = os.path.join(datadir, "tetris", "scenarios", "scheduler",
                              "{}.csv".format(testname))
-    out_name = "{}_{}.out".format("dac", testname)
+    out_name = "{}_{}.out".format(sched_l, testname)
     out_path = os.path.join(datadir, out_name)
     tetris_base = os.path.join(datadir, "tetris")
     subprocess.check_call([
         "pykpn", "tetris", "tetris_base={}".format(tetris_base),
-        "scenario={}".format(input_scn), "scheduler=DAC", "mode=single",
-        "platform=exynos", "output={}".format(out_path)
+        "scenario={}".format(input_scn), "scheduler={}".format(scheduler),
+        "mode={}".format(mode), "platform=exynos", "output={}".format(out_path)
     ], cwd=datadir)
 
     assert filecmp.cmp(os.path.join(expected_dir, out_name), out_path,
                        shallow=False)
+
+
+def test_tetris_dac(datadir, expected_dir, tetris_scheduler_test):
+    run_tetris(datadir, expected_dir, "DAC", tetris_scheduler_test, 'single')
+
+def test_tetris_dac_2(datadir, expected_dir, tetris_scheduler_test):
+    run_tetris(datadir, expected_dir, "DAC-2", tetris_scheduler_test, 'single')
