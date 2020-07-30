833f37d17ee14bf09f1a6ac8843c56e19d273dba
Robert Khasanov <robert.khasanov@tu-dresden.de>
2020-07-30 14:08:10 +0200

    Added wwt15 tests

--- test/test_tetris.py
+++ test/test_tetris.py
@@ -22,19 +22,31 @@ def tetris_scheduler_test(request):
     return request.param
 
 
-def run_tetris(datadir, expected_dir, scheduler, scenario, mode):
+@pytest.fixture(params=["hog-1.csv"])
+def tetris_wwt15_opt_test(request):
+    return request.param
+
+
+@pytest.fixture(params=["wwt15_lr=['R','D','RDP']"])
+def tetris_wwt15_option(request):
+    return request.param
+
+
+def run_tetris(datadir, expected_dir, scheduler, scenario, mode, options="",
+               file_suffix=""):
+    print("Scenario: ", scenario)
     testname = os.path.splitext(scenario)[0]
     sched_l = scheduler.lower()
     input_scn = os.path.join(datadir, "tetris", "scenarios", "scheduler",
                              "{}.csv".format(testname))
-    out_name = "{}_{}.out".format(sched_l, testname)
+    out_name = "{}_{}{}.out".format(sched_l, testname, file_suffix)
     out_path = os.path.join(datadir, out_name)
     tetris_base = os.path.join(datadir, "tetris")
-    subprocess.check_call([
-        "pykpn", "tetris", "tetris_base={}".format(tetris_base),
-        "scenario={}".format(input_scn), "scheduler={}".format(scheduler),
-        "mode={}".format(mode), "platform=exynos", "output={}".format(out_path)
-    ], cwd=datadir)
+    cmd = ("pykpn " + "tetris " + "tetris_base={} ".format(tetris_base) +
+           "scenario={} ".format(input_scn) +
+           "scheduler={} ".format(scheduler) + "mode={} ".format(mode) +
+           "platform=exynos " + options + " " + "output={} ".format(out_path))
+    subprocess.check_call(cmd.split(), cwd=datadir)
 
     assert filecmp.cmp(os.path.join(expected_dir, out_name), out_path,
                        shallow=False)
@@ -43,8 +55,19 @@ def run_tetris(datadir, expected_dir, scheduler, scenario, mode):
 def test_tetris_dac(datadir, expected_dir, tetris_scheduler_test):
     run_tetris(datadir, expected_dir, "DAC", tetris_scheduler_test, 'single')
 
+
 def test_tetris_dac_2(datadir, expected_dir, tetris_scheduler_test):
     run_tetris(datadir, expected_dir, "DAC-2", tetris_scheduler_test, 'single')
 
+
 def test_tetris_fast(datadir, expected_dir, tetris_scheduler_test):
     run_tetris(datadir, expected_dir, "FAST", tetris_scheduler_test, 'single')
+
+
+def test_tetris_wwt15(datadir, expected_dir, tetris_scheduler_test):
+    run_tetris(datadir, expected_dir, "WWT15", tetris_scheduler_test, 'single')
+
+
+def test_tetris_wwt15_rdp(datadir, expected_dir, tetris_wwt15_opt_test):
+    run_tetris(datadir, expected_dir, "WWT15", tetris_wwt15_opt_test, 'single',
+               options="wwt15_lr=['R','D','RDP']", file_suffix="_rdp")
