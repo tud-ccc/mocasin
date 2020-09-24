9e26478c5b875727e76ed2806e86203374711f75
Andrés Goens <andres.goens@tu-dresden.de>
2020-09-24 17:10:56 +0200

    moved exynos 990 and mppa_coolidge out of tgff file

---
+++ pykpn/platforms/exynos990.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2020 TU Dresden
+# All Rights Reserved
+#
+# Authors: Felix Teweleit, Andres Goens
+from pykpn.common.platform import Platform
+from pykpn.platforms.platformDesigner import PlatformDesigner
+
+class DesignerPlatformExynos990(Platform):
+    def __init__(self, processor_1, processor_2, processor_3, processor_4, name="exynos-990"):
+        super(DesignerPlatformExynos990, self).__init__(name)
+        designer = PlatformDesigner(self)
+
+        designer.setSchedulingPolicy('FIFO', 1000)
+        designer.newElement("test_chip")
+
+        # cluster 0 with l2 cache
+        designer.addPeClusterForProcessor("cluster_0",
+                                          processor_1.to_pykpn_processor(),
+                                          2)
+        # Add L1/L2 caches
+        designer.addCacheForPEs("cluster_0", 5, 7, float('inf'), float('inf'), frequencyDomain=6000000.0, name='L1')
+        designer.addCommunicationResource("lvl2_cl0",
+                                          ["cluster_0"],
+                                          225,
+                                          300,
+                                          float('inf'),
+                                          float('inf'),
+                                          frequencyDomain=600000000.0)
+
+        # cluster 1, with l2 cache
+        designer.addPeClusterForProcessor("cluster_1",
+                                          processor_2.to_pykpn_processor(),
+                                          2)
+        # Add L1/L2 caches
+        designer.addCacheForPEs("cluster_1", 5, 7, float('inf'), float('inf'), frequencyDomain=6670000.0, name='L1')
+        designer.addCommunicationResource("lvl2_cl1",
+                                          ["cluster_1"],
+                                          225,
+                                          300,
+                                          float('inf'),
+                                          float('inf'),
+                                          frequencyDomain=667000000.0)
+
+        # cluster 2, with l2 cache
+        designer.addPeClusterForProcessor("cluster_2",
+                                          processor_3.to_pykpn_processor(),
+                                          4)
+        # Add L1/L2 caches
+        designer.addCacheForPEs("cluster_2", 5, 7, float('inf'), float('inf'), frequencyDomain=6670000.0, name='L1')
+        designer.addCommunicationResource("lvl2_cl2",
+                                          ["cluster_2"],
+                                          225,
+                                          300,
+                                          float('inf'),
+                                          float('inf'),
+                                          frequencyDomain=667000000.0)
+
+        # RAM connecting all clusters
+        designer.addCommunicationResource("RAM",
+                                          ["cluster_0", "cluster_1", "cluster_2"],
+                                          800,
+                                          2000,
+                                          float('inf'),
+                                          float('inf'),
+                                          frequencyDomain=6000000.0)
+
+        # single GPU
+        designer.addPeClusterForProcessor("GPU",
+                                          processor_4.to_pykpn_processor(),
+                                          1)
+        designer.addCacheForPEs("GPU", 5, 7, float('inf'), float('inf'), frequencyDomain=6670000.0, name='GPU_MEM')
+
+        # another memory, simulating BUS
+        designer.addCommunicationResource("BUS",
+                                          ["cluster_0", "cluster_1", "cluster_2", "GPU"],
+                                          2000,
+                                          6000,
+                                          float('inf'),
+                                          float('inf'),
+                                          frequencyDomain=4000000.0)
+
+        designer.finishElement()
---
+++ pykpn/platforms/mppa_coolidge.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2020 TU Dresden
+# All Rights Reserved
+#
+# Authors: Felix Teweleit, Andres Goens
+
+from pykpn.common.platform import Platform
+from pykpn.platforms.topologies import fullyConnectedTopology
+from pykpn.platforms.platformDesigner import PlatformDesigner
+
+class DesignerPlatformCoolidge(Platform):
+    #The topology and latency numbers (!) of this should come from the MPPA3 Coolidge
+    #sheet published by Kalray
+    def __init__(self, processor_1, processor_2, name="coolidge"):
+        super(DesignerPlatformCoolidge, self).__init__(name)
+
+        designer = PlatformDesigner(self)
+        designer.setSchedulingPolicy('FIFO', 1000)
+        designer.newElement('coolidge')
+
+        #create five chips with 16 cores, NoC, +Security Core
+        for i in range(0, 5):
+            designer.newElement('cluster_{0}'.format(i))
+
+            designer.addPeClusterForProcessor(f'cluster_{i}_0', processor_1.to_pykpn_processor(), 16)
+
+            topology = fullyConnectedTopology(['processor_{0}'.format(i * 17), 'processor_{0}'.format(i * 17 + 1),
+                                               'processor_{0}'.format(i * 17 + 2), 'processor_{0}'.format(i * 17 + 3),
+                                               'processor_{0}'.format(i * 17 + 4), 'processor_{0}'.format(i * 17 + 5),
+                                               'processor_{0}'.format(i * 17 + 6), 'processor_{0}'.format(i * 17 + 7),
+                                               'processor_{0}'.format(i * 17 + 8), 'processor_{0}'.format(i * 17 + 9),
+                                               'processor_{0}'.format(i * 17 + 10), 'processor_{0}'.format(i * 17 + 11),
+                                               'processor_{0}'.format(i * 17 + 12), 'processor_{0}'.format(i * 17 + 13),
+                                               'processor_{0}'.format(i * 17 + 14), 'processor_{0}'.format(i * 17 + 15)
+                                               ])
+
+            designer.createNetworkForCluster(f'cluster_{i}_0', f'noc_{i}', topology, sd, 40000.0, 100, 150, 100, 60)
+
+            designer.addPeClusterForProcessor(f'cluster_{i}_1', processor_2.to_pykpn_processor(), 1)
+
+            designer.addCommunicationResource(f'L2_{i}', [f'cluster_{i}_0', f'cluster_{i}_1'], 500, 1500,
+                                              float('inf'), float('inf'), frequencyDomain=600000.0)
+
+            designer.finishElement()
+
+        designer.addCommunicationResource("RAM", ['chip_0', 'chip_1', 'chip_2', 'chip_3', 'chip_4'],
+                                          1000, 3000, float('inf'), float('inf'), frequencyDomain=10000)
+
+        designer.finishElement()
