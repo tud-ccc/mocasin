4391ac366769626a5b382692be4c526cc6d58ecf
Andres Goens <goens@goenshome>
2020-11-04 18:47:46 +0100

    Added a ParserNotFoundException for handling cleanly when a parser was not found, documented the parse_multirun task function, added a default behavior for empty path being the latest multirun.

--- pykpn/tasks/parse_multirun.py
+++ pykpn/tasks/parse_multirun.py
@@ -7,10 +7,14 @@
 
 import logging
 import hydra
+from os import scandir
 from pykpn.util.multirun_reader import read_multirun
 from importlib import import_module
 
 log = logging.getLogger(__name__)
+
+class ParserNotFoundException(Exception):
+    pass
 
 _parsers = {
     'mapper_statistics': (
@@ -32,15 +36,15 @@ def available_parsers():
 
 
 def load_parser(name):
-   """Loads an individual task.
+   """Loads an individual parser.
 
-   :param task: name of the task to be executed
+   :param name: name of the parser to be executed
    :return:
    """
 
    if name not in _parsers:
        log.error(f"Unknown parser: {name}. Available parsers are:\n\n" + available_parsers())
-       raise RuntimeError()
+       raise ParserNotFoundException()
 
    else:
        # load the parser
@@ -53,15 +57,36 @@ def load_parser(name):
 
 @hydra.main(config_path='../conf', config_name='parse_multirun')
 def parse_multirun(cfg):
+    """This task parses the directory structure of a Hydra multirun execution.
+    It generates a csv file with all options given explicitly to the multirun as columns,
+    and every row corresponds to a single execution of the multirun sweep.
+
+    The task can be configured in a modular fashion by adding different parsers. The parsers
+    will read a concrete file and generate results to be added as columns in the output csv.
+    These parsers are intended to be usecase-specific. The parsers to be used are given as a list
+    in the hydra conifuration file.
+
+    Hydra parameters:
+     - path: the path of the root of the multirun directory. If empty it will
+     search for the latest (by name) in ./multirun/*
+     - parsers: a list of the parses to be used
+
+    :param cfg: Hydra (Omegaconf) config file.
+    """
     try:
         parsers = []
         for parser in cfg['parsers']:
             parsers.append(load_parser(parser))
         path = cfg['path']
+        if path == "":
+            path = sorted([x.path for x in scandir("multirun/") if x.is_dir()])[-1]
         read_multirun(path,parsers)
-    except Exception:
-        print("parse_multirun usage: pykpn parse_multirun path=<multirun_root_directory> parsers=[<parser list>]\n")
-        print("For example, to look at the mapper statistics after a multirun mapping:")
-        print("pykpn path=parse_multirun examples/multirun/1999-12-31-23-59-59 parsers=[mapper_statistics]")
-        print("-------------------------------------------------------------------------------------------\n")
-        raise(Exception)
+    except ParserNotFoundException:
+        print_usage()
+        raise ParserNotFoundException()
+
+def print_usage():
+    print("parse_multirun usage: pykpn parse_multirun path=<multirun_root_directory> parsers=[<parser list>]\n")
+    print("For example, to look at the mapper statistics after a multirun mapping:")
+    print("pykpn path=parse_multirun examples/multirun/1999-12-31-23-59-59 parsers=[mapper_statistics]")
+    print("-------------------------------------------------------------------------------------------\n")
