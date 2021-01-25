719421742f2d24bfacd17bba89206750b7c0560e
Christian Menard <christian.menard@tu-dresden.de>
2021-01-25 17:11:11 +0100

    make mocasin, mocasin.mapper and mocasin.conf namespace packages

--- mocasin/__init__.py
+++ mocasin/__init__.py
@@ -2,3 +2,5 @@
 # Licensed under the ISC license (see LICENSE.txt)
 #
 # Authors: Christian Menard
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
--- mocasin/conf/__init__.py
+++ mocasin/conf/__init__.py
@@ -1,3 +1,8 @@
-"""
-Init file for conf package is required since hydra 1.0
-"""
+# Copyright (C) 2021 TU Dresden
+# Licensed under the ISC license (see LICENSE.txt)
+#
+# Authors: Christian Menard
+
+# Init file for conf package is required since hydra 1.0
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
