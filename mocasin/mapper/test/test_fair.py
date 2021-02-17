d9fbf6fb88ada9596d56a287b52ca600ef04d67d
Christian Menard <christian.menard@tu-dresden.de>
2021-02-17 14:32:50 +0100

    fix fair mapper tests

--- mocasin/mapper/test/test_fair.py
+++ mocasin/mapper/test/test_fair.py
@@ -5,31 +5,23 @@
 
 from mocasin.mapper.fair import StaticCFS, gen_trace_summary
 from mocasin.common.mapping import Mapping
-from mocasin.common.trace import TraceSegment
+from mocasin.common.trace import ComputeSegment, DataflowTrace
 
 
-class MockTraceGenerator(object):
+class MockTrace(DataflowTrace):
     def __init__(self, proc_names, core_types, lookup_function, max_length=1):
         self.cores = core_types
         self.procs = proc_names
         self.lookup = lookup_function
         self.max_length = max_length
-        self.num_lookups = {}
-        self.reset()
 
-    def reset(self):
-        for core in self.cores:
-            for proc in self.procs:
-                self.num_lookups[(proc, core)] = 0
+    def get_trace(self, proc_name):
+        for _ in range(0, self.max_length):
+            processor_cycles = {}
+            for core in self.cores:
+                processor_cycles[core] = self.lookup((core, proc_name))
 
-    def next_segment(self, proc_name, core_type):
-        self.num_lookups[(proc_name, core_type)] += 1
-        if self.num_lookups[(proc_name, core_type)] >= self.max_length:
-            return TraceSegment(terminate=True)
-        else:
-            return TraceSegment(
-                process_cycles=self.lookup((proc_name, core_type))
-            )
+            yield ComputeSegment(processor_cycles)
 
 
 def test_gen_trace_summary(graph, platform):
@@ -38,20 +30,20 @@ def test_gen_trace_summary(graph, platform):
     proc_names = [proc.name for proc in graph.processes()]
     core_types = [core.type for core in platform.processors()]
     const_func = lambda _: const_value
-    trace = MockTraceGenerator(
-        proc_names, core_types, const_func, max_length=num_iter
-    )
+    trace = MockTrace(proc_names, core_types, const_func, max_length=num_iter)
     trace_summary = gen_trace_summary(graph, platform, trace)
+    for proc in proc_names:
+        assert len(list(trace.get_trace(proc))) == num_iter
     for core in core_types:
         for proc in graph.processes():
-            assert trace_summary[(proc, core)] == const_value * (num_iter - 1)
+            assert trace_summary[(proc, core)] == const_value * num_iter
 
     for core in core_types:
         for proc in graph.processes():
             single_comb = (
                 lambda x: (x[0] == core) * (x[1] == proc.name) * const_value
             )
-            trace = MockTraceGenerator(
+            trace = MockTrace(
                 proc_names, core_types, single_comb, max_length=num_iter
             )
             trace_summary = gen_trace_summary(graph, platform, trace)
@@ -62,8 +54,8 @@ def test_gen_trace_summary(graph, platform):
                     assert (
                         other_core == core
                         and other_proc == proc
-                        and val == (num_iter - 1) * const_value
-                    ) or (val == 0)
+                        and val == num_iter * const_value
+                    ) or val == 0
 
 
 def test_map_to_core(graph, platform):
