0e624260b26d65025b799f6f50a497725fd1d749
Andres Goens <goens@goenshome>
2021-02-18 09:51:55 +0100

    added test for misaligned files in symmetries

--- test/test_symmetries.py
+++ test/test_symmetries.py
@@ -92,3 +92,51 @@ def test_symmetries_tgff_small(datadir, small_platform):
         os.path.join(datadir, "python/best_time.txt"),
         shallow=False,
     )
+
+
+def test_misaligned(datadir):
+    subprocess.check_call(
+        [
+            "mocasin",
+            "generate_mapping",
+            "graph=tgff_reader",
+            "tgff.directory=tgff/e3s-0.9",
+            "tgff.file=auto-indust-cords.tgff",
+            "platform=odroid",
+            "representation=Symmetries",
+            "mapper=genetic",
+            f"outdir={datadir}/misaligned/",
+            "platform.symmetries_json=platform/symmetries/generic_mesh.json",
+            "trace=tgff_reader",
+        ],
+        cwd=datadir,
+    )
+
+    subprocess.check_call(
+        [
+            "mocasin",
+            "generate_mapping",
+            "graph=tgff_reader",
+            "tgff.directory=tgff/e3s-0.9",
+            "tgff.file=auto-indust-cords.tgff",
+            "platform=odroid",
+            "representation=Symmetries",
+            "mapper=genetic",
+            f"outdir={datadir}/correct/",
+            f"platform.symmetries_json=platform/symmetries/odroid.json",
+            "trace=tgff_reader",
+        ],
+        cwd=datadir,
+    )
+    assert filecmp.cmp(
+        os.path.join(datadir, "misaligned/best_time.txt"),
+        os.path.join(datadir, "correct/best_time.txt"),
+        shallow=False,
+    )
+
+    # check that misaliged call does not change the json file
+    assert not filecmp.cmp(
+        f"../examples/platform/symmetries/generic_mesh.json",
+        f"../examples/platform/symmetries/odroid.json",
+        shallow=False,
+    )
