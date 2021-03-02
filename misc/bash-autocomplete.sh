a92f76c383d2f3f75dd0bc6ccfca5670fa4a42cd
Christian Menard <christian.menard@tu-dresden.de>
2021-03-02 09:18:16 +0100

    add a few comments and exit safely when mocasin is not installed

--- misc/bash-autocomplete.sh
+++ misc/bash-autocomplete.sh
@@ -11,9 +11,15 @@ mocasin_bash_completion()
         return
     fi
 
-    choices=$(COMP_POINT=$COMP_POINT COMP_LINE=$COMP_LINE COMP_CWORD=$COMP_CWORD "mocasin" "autocomplete")
-    word=${words[$COMP_CWORD]}
+    # We can only autocomplete if the mocasin command is avalaiable
+    if ! command -v mocasin &> /dev/null; then
+        return
+    fi
 
+    # get all choices by running `mocasin autocomplete`
+    choices=$(COMP_POINT=$COMP_POINT COMP_LINE=$COMP_LINE COMP_CWORD=$COMP_CWORD "mocasin" "autocomplete")
+
+    # print debug information if requested
     if [ "$MOCASIN_COMP_DEBUG" == "1" ]; then
         printf "\n"
         printf "COMP_LINE='$COMP_LINE'\n"
@@ -24,6 +30,8 @@ mocasin_bash_completion()
         printf "\t%s\n" ${choices[@]}
     fi
 
+    # prepare a reply
+    word=${words[$COMP_CWORD]}
     COMPREPLY=($( compgen -o nospace -o default -W "$choices" -- "$word" ));
 }
 
