7f3c388e78f8929888ee1c3cc697a82dfdb33521
Andres Goens <andres@goens.org>
2021-03-09 13:31:40 +0100

    added some sanity-check tests for static costs. token-size dependence fails for some platforms (generic_mesh,haec, coolidge)

---
+++ mocasin/platforms/test/test_static_costs.py
@@ -0,0 +1,8 @@
+def test_static_costs(platform):
+    sizes = [1,8,64,1024]
+    for prim in platform.primitives():
+        for src in prim.producers:
+            for tgt in prim.consumers:
+                static_costs = [prim.static_costs(src,tgt,token_size=size) for size in sizes]
+                for i in range(len(sizes)-1):
+                    assert  static_costs[i] <= static_costs[i+1] #costs don't decrease for larger tokens
