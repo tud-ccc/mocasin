5adbd256903b00b64a684701e697b8f6941f7660
Christian Menard <christian.menard@tu-dresden.de>
2021-03-12 15:20:42 +0100

    add file header and mark test as xfail

--- mocasin/platforms/test/test_static_costs.py
+++ mocasin/platforms/test/test_static_costs.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2021 TU Dresden
+# Licensed under the ISC license (see LICENSE.txt)
+#
+# Authors: Andrés Goens
+
+import pytest
+
+@pytest.xfail("There is a bug in NoCs produced by the platform designer")
 def test_static_costs(platform):
     sizes = [1, 8, 64, 1024]
     for prim in platform.primitives():
@@ -8,6 +16,5 @@ def test_static_costs(platform):
                     for size in sizes
                 ]
                 for i in range(len(sizes) - 1):
-                    assert (
-                        static_costs[i] <= static_costs[i + 1]
-                    )  # costs don't decrease for larger tokens
+                    # costs don't decrease for larger tokens
+                    assert static_costs[i] <= static_costs[i + 1]
