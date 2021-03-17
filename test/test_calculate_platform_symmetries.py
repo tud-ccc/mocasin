2feecca998027de83b2464f838d92c3030b13d77
Andres Goens <andres@goens.org>
2021-03-17 10:34:37 +0100

    black seems to want to even mess with my comments...

--- test/test_calculate_platform_symmetries.py
+++ test/test_calculate_platform_symmetries.py
@@ -9,7 +9,7 @@ import os
 import pytest
 
 # this comparison test is broken, as it will reject correctly calculated symmetries e.g.
-#if a different but still valid BSGS is chosen. This should be fixed in !62
+# if a different but still valid BSGS is chosen. This should be fixed in !62
 @pytest.mark.xfail
 @pytest.mark.parametrize("mpsym", [True, False])
 def test_calculate_platform_symmetries(
