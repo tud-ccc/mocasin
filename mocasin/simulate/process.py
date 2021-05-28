5c6c06322bdc8e26a6f4f2d230ac7cfe9501f2b2
Christian Menard <christian.menard@tu-dresden.de>
2021-05-28 10:47:05 +0200

    simulate: add wait_for_initial_tokens configuration key

--- mocasin/simulate/process.py
+++ mocasin/simulate/process.py
@@ -432,9 +432,11 @@ class RuntimeDataflowProcess(RuntimeProcess):
             within the same system.
         process_trace (generator): a generator of process trace segments
         app (RuntimeApplication): the application this process is part of
+        wait_for_initial_tokens (bool): If true, the process only starts if
+            initial tokens (first reads in the trace) are available.
     """
 
-    def __init__(self, name, app):
+    def __init__(self, name, app, wait_for_initial_tokens=False):
         super().__init__(name, app)
         log.debug(
             "initialize new dataflow runtime process (%s)", self.full_name
@@ -448,7 +450,7 @@ class RuntimeDataflowProcess(RuntimeProcess):
 
         # a seekable iterator over all segments in the process trace
         self._trace = more_itertools.seekable(
-            app_trace.get_trace(name), maxlen=32
+            app_trace.get_trace(name), maxlen=16
         )
 
         self._current_segment = None
@@ -462,6 +464,8 @@ class RuntimeDataflowProcess(RuntimeProcess):
         # lets the workload method know whether it is run for the first time
         # or whether it is resumed
         self._is_running = False
+
+        self._wait_for_initial_tokens = wait_for_initial_tokens
 
     def connect_to_incomming_channel(self, channel):
         """Connect the process to an incoming runtime channel
@@ -505,37 +509,40 @@ class RuntimeDataflowProcess(RuntimeProcess):
 
         self.processor = None
 
-        # collect all initial read segments in the traces
-        initial_read_segments = []
-        for segment in self._trace:
-            if segment.segment_type == SegmentType.READ_TOKEN:
-                # collect read segments
-                initial_read_segments.append(segment)
-            else:
-                break  # abort at the fist occurence of any other segment
-        # reset the seekable iterator to its initial state
-        self._trace.seek(0)
+        if self._wait_for_initial_tokens:
+            # collect all initial read segments in the traces
+            initial_read_segments = []
+            for segment in self._trace:
+                if segment.segment_type == SegmentType.READ_TOKEN:
+                    # collect read segments
+                    initial_read_segments.append(segment)
+                else:
+                    break  # abort at the fist occurrence of any other segment
+                # reset the seekable iterator to its initial state
+            self._trace.seek(0)
 
-        # iterate over the initial read segments and see if we would need
-        # to block and wait for tokens in the channels
-        channel_token_pairs = []
-        for segment in initial_read_segments:
-            channel = self._channels[segment.channel]
-            if not channel.can_consume(self, segment.num_tokens):
-                channel_token_pairs.append((channel, segment.num_tokens))
-                self._log.debug(
-                    f"Process blocks because it needs {segment.num_tokens} "
-                    f"initial tokens in channel {segment.channel}"
+            # iterate over the initial read segments and see if we would need
+            # to block and wait for tokens in the channels
+            channel_token_pairs = []
+            for segment in initial_read_segments:
+                channel = self._channels[segment.channel]
+                if not channel.can_consume(self, segment.num_tokens):
+                    channel_token_pairs.append((channel, segment.num_tokens))
+                    self._log.debug(
+                        f"Process blocks because it needs {segment.num_tokens} "
+                        f"initial tokens in channel {segment.channel}"
+                    )
+
+            if len(channel_token_pairs) > 0:
+                self.env.process(
+                    self._do_wait_for_initial_tokens(channel_token_pairs)
                 )
-
-        if len(channel_token_pairs) > 0:
-            self.env.process(self._wait_for_initial_tokens(channel_token_pairs))
-            self._transition("BLOCKED")
-            return
+                self._transition("BLOCKED")
+                return
 
         self._transition("READY")
 
-    def _wait_for_initial_tokens(self, channel_token_pairs):
+    def _do_wait_for_initial_tokens(self, channel_token_pairs):
         # Keep waiting in a loop until enough tokens can be found in all
         # channels to support the initial needs of this process.
         while not all((c.can_consume(self, n) for c, n in channel_token_pairs)):
