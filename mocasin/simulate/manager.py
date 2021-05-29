6d0503f07c54faacb103c912d8b0b65a4fd7ebd7
Robert Khasanov <robert.khasanov@tu-dresden.de>
2021-05-29 15:31:53 +0200

    simulate: manager: Created ManagerStatistics class

--- mocasin/simulate/manager.py
+++ mocasin/simulate/manager.py
@@ -3,11 +3,143 @@
 #
 # Authors: Robert Khasanov, Christian Menard
 
+import csv
+from dataclasses import asdict, dataclass
 import logging
 
 from mocasin.simulate.adapter import SimulateLoggerAdapter
 
 log = logging.getLogger(__name__)
+
+
+@dataclass
+class ManagerStatisticsApplicationEntry:
+    """A log entry of the application scheduling with the runtime manager."""
+
+    name: str
+    arrival: int
+    deadline: int
+
+    accepted: bool = None
+    expected_end_time: float = None
+
+    start_time: int = None
+    end_time: int = None
+    missed_deadline: bool = None
+
+    def to_dict(self):
+        """Get the dict representation."""
+        return asdict(self)
+
+
+@dataclass
+class ManagerStatisticsActivationEntry:
+    """A log entry of the runtime manager activation."""
+
+    activation_time: int
+    num_new_apps: int
+    num_accepted_apps: int
+    num_prev_apps: int
+    scheduling_time: int
+
+    def to_dict(self):
+        """Get the dict representation."""
+        return asdict(self)
+
+
+class ManagerStatistics:
+    """Collection and export of statistics after simulation."""
+
+    def __init__(self):
+        self.applications = {}
+        self.activations = []
+
+    def new_application(self, name, arrival=None, deadline=None):
+        """Create an application entry."""
+        entry = ManagerStatisticsApplicationEntry(
+            name=name, arrival=arrival, deadline=deadline
+        )
+        self.applications[entry.name] = entry
+        return entry
+
+    def new_activation(
+        self, activation_time, num_new, num_accepted, num_prev, scheduling_time
+    ):
+        """Create an activation entry."""
+        entry = ManagerStatisticsActivationEntry(
+            activation_time=activation_time,
+            num_new_apps=num_new,
+            num_accepted_apps=num_accepted,
+            num_prev_apps=num_prev,
+            scheduling_time=scheduling_time,
+        )
+        self.activations.append(entry)
+        return entry
+
+    def total_applications(self):
+        """Returns the total number of applications."""
+        return len(self.applications)
+
+    def total_accepted(self):
+        """Returns the number of accepted applications."""
+        return sum([1 for x in self.applications.values() if x.accepted])
+
+    def total_rejected(self):
+        """Returns the number of rejected applications."""
+        return self.total_applications() - self.total_accepted()
+
+    def total_missed(self):
+        """Returns the number of applications missed the deadline."""
+        return sum([1 for x in self.applications.values() if x.missed_deadline])
+
+    def total_activations(self):
+        """Returns the total number of activations."""
+        return len(self.activations)
+
+    def total_scheduling_time(self):
+        """Returns the total scheduling time."""
+        return sum(x.scheduling_time for x in self.activations)
+
+    def average_scheduling_time(self):
+        """Returns the average scheduling time."""
+        return self.total_scheduling_time() / self.total_activations()
+
+    def find_application(self, name):
+        """Find the entry by the applications name."""
+        return self.applications.get(name, None)
+
+    def dump_applications(self, filename):
+        """Dump application entries to a CSV file."""
+        fieldnames = [
+            "name",
+            "arrival",
+            "deadline",
+            "accepted",
+            "expected_end_time",
+            "start_time",
+            "end_time",
+            "missed_deadline",
+        ]
+        with open(filename, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for entry in self.applications.values():
+                writer.writerow(entry.to_dict())
+
+    def dump_activations(self, filename):
+        """Dump activation entries to a CSV file."""
+        fieldnames = [
+            "activation_time",
+            "num_new_apps",
+            "num_accepted_apps",
+            "num_prev_apps",
+            "scheduling_time",
+        ]
+        with open(filename, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for entry in self.activations:
+                writer.writerow(entry.to_dict())
 
 
 class RuntimeManager:
