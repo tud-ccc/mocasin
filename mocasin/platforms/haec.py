a47d0e8e041dbfcf389e304f292d7bf30edc76f9
Julian Robledo Mejia <julian.robledo_mejia@tu-dresden.de>
2021-11-02 22:10:22 +0100

    Adapted Haec platform to the new platform designer

--- mocasin/platforms/haec.py
+++ mocasin/platforms/haec.py
@@ -1,12 +1,17 @@
 # Copyright (C) 2020 TU Dresden
 # Licensed under the ISC license (see LICENSE.txt)
 #
-# Authors: Felix Teweleit, Andres Goens
+# Authors: Felix Teweleit, Andres Goens, Julian Robledo
 
-from mocasin.common.platform import Platform, Processor
+from mocasin.common.platform import (
+    Platform,
+    Processor,
+    CommunicationResourceType,
+    CommunicationResource,
+    FrequencyDomain)
 from mocasin.platforms.platformDesigner import PlatformDesigner
 from hydra.utils import instantiate
-from mocasin.platforms.topologies import meshTopology
+from mocasin.platforms.topologies import meshTopology, fullyConnectedTopology
 from mocasin.platforms.utils import simpleDijkstra as sd
 
 
@@ -28,43 +33,111 @@ class DesignerPlatformHAEC(Platform):
 
         designer = PlatformDesigner(self)
         designer.setSchedulingPolicy("FIFO", 1000)
-        designer.newElement("haec_like")
+        haec = designer.addCluster("haec_like")
+
+        fd = FrequencyDomain("fd_electric", 6000000.0)
+        pl = CommunicationResource(
+            "electric",
+            fd,
+            CommunicationResourceType.PhysicalLink,
+            100,
+            150,
+            100,
+            60
+        )
+
+        clusters = list()
         for i in range(4):
-            designer.addPeClusterForProcessor(f"cluster_{i}", processor_0, 16)
-            processors = [
-                f"processor_{j:04d}" for j in range(i * 16, (i + 1) * 16)
-            ]
-            topology = meshTopology(processors)
-            designer.createNetworkForCluster(
-                f"cluster_{i}",
-                "optic",
+            cluster = designer.addCluster(f"cluster_{i}", haec)
+            nocList = []
+            for j in range(16):
+                pe = designer.addPeToCluster(
+                    cluster,
+                    f"processor_{(j+i*16):04d}",
+                    processor_0.type,
+                    processor_0.frequency_domain,
+                    processor_0.power_model,
+                    processor_0.context_load_cycles,
+                    processor_0.context_store_cycles,
+                )
+                noc = designer.addRouter(
+                    f"noc_{(j+i*16):04d}",
+                    cluster,
+                    readLatency=100,
+                    writeLatency=150,
+                    readThroughput=100,
+                    writeThroughput=60,
+                    frequency=6000000.0,
+                )
+                designer.connectPeToCom(pe, noc)
+                nocList.append(noc)
+
+            topology = meshTopology(nocList)
+            designer.createNetworkForRouters(
+                "electric",
+                nocList,
                 topology,
                 sd,
-                6000000.0,
-                100,
-                150,
-                100,
-                60,
+                pl
             )
+            clusters.append(cluster)
 
-        for i in range(4 - 1):
-            designer.addCommunicationResource(
+        wirelessList = list()
+        for i in range(4):
+            wireless = designer.addRouter(
                 f"wireless_{i}",
-                [f"cluster_{i}", f"cluster_{i+1}"],
-                200,
-                300,
-                float("inf"),
-                float("inf"),
-                frequencyDomain=6000000.0,
+                clusters[i],
+                readLatency=200,
+                writeLatency=300,
+                readThroughput=float("inf"),
+                writeThroughput=float("inf"),
+                frequency=6000000.0,
             )
 
-        designer.addCommunicationResource(
-            f"RAM",
-            [f"cluster_{i}" for i in range(4)],
-            2000,
-            3000,
+            processors = designer.getPesInCluster(clusters[i])
+            for pe in processors:
+                designer.connectPeToCom(pe, wireless)
+            wirelessList.append(wireless)
+
+        pl = CommunicationResource(
+            "wireless",
+            fd,
+            CommunicationResourceType.PhysicalLink,
+            200,
+            300,
             float("inf"),
             float("inf"),
-            frequencyDomain=6000000.0,
         )
-        designer.finishElement()
+        topology = fullyConnectedTopology(wirelessList)
+        designer.createNetworkForRouters(
+            "optic",
+            wirelessList,
+            topology,
+            sd,
+            pl
+        )
+
+        ram = designer.addStorage(
+            "RAM",
+            haec,
+            readLatency=2000,
+            writeLatency=3000,
+            readThroughput=float("inf"),
+            writeThroughput=float("inf"),
+            frequency=6000000.0,
+        )
+
+        for i in range(4):
+            cache = designer.addStorage(
+                f"cache_{i}",
+                clusters[i],
+                readLatency=0,
+                writeLatency=0,
+                readThroughput=float("inf"),
+                writeThroughput=float("inf"),
+                frequency=6000000.0,
+            )
+            processors = designer.getPesInCluster(clusters[i])
+            for pe in processors:
+                designer.connectPeToCom(pe, cache)
+            designer.connectStorageLevels(cache, ram)
