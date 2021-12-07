d0be5d44de3c710852eff0f5e7590f26792be70d
Julian Robledo Mejia <julian.robledo@tu-dresden.com>
2021-12-07 15:44:32 +0100

    Updated exynos_2chips platform with new desinger

--- mocasin/platforms/exynos_2chips.py
+++ mocasin/platforms/exynos_2chips.py
@@ -14,11 +14,14 @@ from mocasin.common.platform import (
     CommunicationPhase,
     Primitive,
 )
-
+from mocasin.platforms.platformDesigner import PlatformDesigner
 
 class Exynos2Chips(Platform):
     def __init__(self):
         super(Exynos2Chips, self).__init__("Exynos2Chips")
+
+        designer = PlatformDesigner(self)
+        exynos2chips = designer.addCluster("exynos2chips")
 
         # Frequency domains
         fd_a7 = FrequencyDomain("fd_a7", 1400000000)
@@ -26,303 +29,112 @@ class Exynos2Chips(Platform):
         fd_l3 = FrequencyDomain("fd_l3", 1400000000)
         fd_ram = FrequencyDomain("fd_ram", 933000000)
 
-        # Processors
-        for i in range(0, 4):
-            self.add_processor(Processor("PE%02d" % i, "ARM_CORTEX_A7", fd_a7))
-        for i in range(4, 8):
-            self.add_processor(
-                Processor("PE%02d" % i, "ARM_CORTEX_A15", fd_a15)
-            )
-        for i in range(8, 12):
-            self.add_processor(Processor("PE%02d" % i, "ARM_CORTEX_A7", fd_a7))
-        for i in range(12, 16):
-            self.add_processor(
-                Processor("PE%02d" % i, "ARM_CORTEX_A15", fd_a15)
-            )
-
         # Schedulers
-        sp = SchedulingPolicy("FIFO", 1000)
-        for i in range(0, 16):
-            self.add_scheduler(
-                Scheduler(
-                    "sched%02d" % i, [self.find_processor("PE%02d" % i)], [sp]
+        designer.setSchedulingPolicy("FIFO", 1000)
+
+        # Processors
+        processor_a7 = Processor("PE0", "ARM_CORTEX_A7", fd_a7, None)
+        processor_a15 = Processor("PE1", "ARM_CORTEX_A15", fd_a15, None)
+
+        num_chips = 2
+        num_clusters = 2
+        num_pes = 4
+
+        chips = list()
+        clusters = list()
+        l3_list = list()
+
+        for i in range(num_chips):
+            chip = designer.addCluster(f"chip{i}", exynos2chips)
+
+            l2_list = list()
+            for j in range(num_clusters):
+                cluster = designer.addCluster(f"cluster{j}_chip{i}", chip)
+                print(j % num_clusters)
+                if not j % num_clusters:
+                    pe_base = processor_a7
+                    fd = fd_a7
+                else:
+                    pe_base = processor_a15
+                    fd = fd_a15
+
+                l1_list = list()
+                for k in range(num_pes):
+                    print(pe_base.name)
+                    # Processors
+                    pe = designer.addPeToCluster(
+                        cluster,
+                        f"processor{k}_cluster{j}_chip{i}",
+                        pe_base.type,
+                        pe_base.frequency_domain,
+                        pe_base.power_model,
+                        pe_base.context_load_cycles,
+                        pe_base.context_store_cycles,
+                    )
+
+                    # L1 Caches
+                    l1 = designer.addStorage(
+                        f"l1_{k}_cluster{j}_chip{i}",
+                        cluster,
+                        readLatency=1,
+                        writeLatency=4,
+                        readThroughput=8,
+                        writeThroughput=8,
+                        frequency=fd.frequency,
+                    )
+
+                    # L1 Primitives
+                    designer.connectPeToCom(pe, l1)
+                    l1_list.append(l1)
+
+                # L2 Caches
+                l2 = designer.addStorage(
+                    f"l2_cluster{j}_chip{i}",
+                    cluster,
+                    readLatency=16,
+                    writeLatency=21,
+                    readThroughput=8,
+                    writeThroughput=8,
+                    frequency=fd.frequency,
                 )
+
+                # L2 Primitives
+                for l1 in l1_list:
+                    designer.connectStorageLevels(l1, l2)
+                l2_list.append(l2)
+                clusters.append(cluster)
+
+            # L3 Caches
+            if i == 0:
+                readLatency = 30
+            elif i == 1:
+                readLatency = 40
+
+            l3 = designer.addStorage(
+                f"l3_chip{i}",
+                chip,
+                readLatency=readLatency,
+                writeLatency=21,
+                readThroughput=8,
+                writeThroughput=8,
+                frequency=fd_l3.frequency,
             )
 
-        # L1 Caches
-        for i in range(0, 4):
-            self.add_communication_resource(
-                Storage(
-                    "L1_PE%02d" % i,
-                    fd_a7,
-                    read_latency=1,
-                    write_latency=4,
-                    read_throughput=8,
-                    write_throughput=8,
-                )
-            )
-        for i in range(4, 8):
-            self.add_communication_resource(
-                Storage(
-                    "L1_PE%02d" % i,
-                    fd_a15,
-                    read_latency=1,
-                    write_latency=4,
-                    read_throughput=8,
-                    write_throughput=8,
-                )
-            )
-        for i in range(8, 12):
-            self.add_communication_resource(
-                Storage(
-                    "L1_PE%02d" % i,
-                    fd_a7,
-                    read_latency=1,
-                    write_latency=4,
-                    read_throughput=8,
-                    write_throughput=8,
-                )
-            )
-        for i in range(12, 16):
-            self.add_communication_resource(
-                Storage(
-                    "L1_PE%02d" % i,
-                    fd_a15,
-                    read_latency=1,
-                    write_latency=4,
-                    read_throughput=8,
-                    write_throughput=8,
-                )
-            )
-
-        # L1 Primitives
-        for i in range(0, 16):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-            produce = CommunicationPhase("produce", [l1], "write")
-            consume = CommunicationPhase("consume", [l1], "read")
-
-            prim = Primitive("prim_L1_PE%02d" % i)
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-            self.add_primitive(prim)
-
-        # L2 Caches
-        l2_C0_A7 = Storage(
-            "L2_C0_A7",
-            fd_a7,
-            read_latency=16,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        l2_C0_A15 = Storage(
-            "L2_C0_A15",
-            fd_a15,
-            read_latency=16,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        l2_C1_A7 = Storage(
-            "L2_C1_A7",
-            fd_a7,
-            read_latency=16,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        l2_C1_A15 = Storage(
-            "L2_C1_A15",
-            fd_a15,
-            read_latency=16,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        self.add_communication_resource(l2_C0_A7)
-        self.add_communication_resource(l2_C0_A15)
-        self.add_communication_resource(l2_C1_A7)
-        self.add_communication_resource(l2_C1_A15)
-
-        # L2 Primitives
-        prim = Primitive("prim_L2_C0_A7")
-        for i in range(0, 4):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase("produce", [l1, l2_C0_A7], "write")
-            consume = CommunicationPhase("consume", [l1, l2_C0_A7], "read")
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
-
-        prim = Primitive("prim_L2_C0_A15")
-        for i in range(4, 8):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase("produce", [l1, l2_C0_A15], "write")
-            consume = CommunicationPhase("consume", [l1, l2_C0_A15], "read")
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
-
-        prim = Primitive("prim_L2_C1_A7")
-        for i in range(8, 12):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase("produce", [l1, l2_C1_A7], "write")
-            consume = CommunicationPhase("consume", [l1, l2_C1_A7], "read")
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
-
-        prim = Primitive("prim_L2_C1_A15")
-        for i in range(12, 16):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase("produce", [l1, l2_C1_A15], "write")
-            consume = CommunicationPhase("consume", [l1, l2_C1_A15], "read")
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
-
-        # L3 Caches
-        l3_C0 = Storage(
-            "L3_C0",
-            fd_l3,
-            read_latency=30,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        l3_C1 = Storage(
-            "L3_C1",
-            fd_l3,
-            read_latency=40,
-            write_latency=21,
-            read_throughput=8,
-            write_throughput=8,
-        )
-        self.add_communication_resource(l3_C0)
-        self.add_communication_resource(l3_C1)
-
-        # L3 Primitives
-        prim = Primitive("prim_L3_C0")
-        for i in range(0, 4):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C0_A7, l3_C0], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C0_A7, l3_C0], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        for i in range(4, 8):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C0_A15, l3_C0], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C0_A15, l3_C0], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
-
-        prim = Primitive("prim_L3_C1")
-        for i in range(8, 12):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C1_A7, l3_C1], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C1_A7, l3_C1], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        for i in range(12, 16):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C1_A15, l3_C1], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C1_A15, l3_C1], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
+            # L3 Primitives
+            for l2 in l2_list:
+                designer.connectStorageLevels(l2, l3)
+            l3_list.append(l3)
+            chips.append(chip)
 
         # RAM
-        ram = Storage(
+        RAM = designer.addStorage(
             "RAM",
-            fd_ram,
-            read_latency=120,
-            write_latency=120,
-            read_throughput=8,
-            write_throughput=8,
+            exynos2chips,
+            readLatency=120,
+            writeLatency=120,
+            readThroughput=8,
+            writeThroughput=8,
+            frequency=fd_ram.frequency,
         )
-        self.add_communication_resource(ram)
-
-        prim = Primitive("prim_RAM")
-        for i in range(0, 4):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C0_A7, l3_C0, ram], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C0_A7, l3_C0, ram], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        for i in range(4, 8):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C0_A15, l3_C0, ram], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C0_A15, l3_C0, ram], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        for i in range(8, 12):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C1_A7, l3_C1, ram], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C1_A7, l3_C1, ram], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        for i in range(12, 16):
-            pe = self.find_processor("PE%02d" % i)
-            l1 = self.find_communication_resource("L1_PE%02d" % i)
-
-            produce = CommunicationPhase(
-                "produce", [l1, l2_C1_A15, l3_C1, ram], "write"
-            )
-            consume = CommunicationPhase(
-                "consume", [l1, l2_C1_A15, l3_C1, ram], "read"
-            )
-            prim.add_producer(pe, [produce])
-            prim.add_consumer(pe, [consume])
-        self.add_primitive(prim)
+        for l3 in l3_list:
+            designer.connectStorageLevels(l3, RAM)
