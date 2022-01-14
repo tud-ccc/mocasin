e170d1edb0f0b1d9fbcc51968dddc66d1d1c18c7
Julian Robledo Mejia <julian.robledo@tu-dresden.com>
2022-01-14 14:09:42 +0100

    Modified platform multi_cluster with the new designer

--- mocasin/platforms/multi_cluster.py
+++ mocasin/platforms/multi_cluster.py
@@ -29,41 +29,71 @@ class DesignerPlatformMultiCluster(Platform):
 
         designer = PlatformDesigner(self)
         designer.setSchedulingPolicy("FIFO", 1000)
-        designer.newElement("multi_cluster")
+        multi_cluster = designer.addCluster("multi_cluster")
 
         # add first cluster with L2 cache
-        designer.addPeClusterForProcessor("cluster_0", processor_0, 2)
-        designer.addCommunicationResource(
+        cluster_0 = designer.addCluster("cluster_0", multi_cluster)
+        pe_list = list()
+        for i in range(2):
+            pe = designer.addPeToCluster(
+                cluster_0,
+                f"processor_{i:02d}_0",
+                processor_0.type,
+                processor_0.frequency_domain,
+                processor_0.power_model,
+                processor_0.context_load_cycles,
+                processor_0.context_store_cycles,
+            )
+            pe_list.append(pe)
+
+        cl0_l2 = designer.addStorage(
             "cl0_l2",
-            ["cluster_0"],
-            100,
-            100,
-            float("inf"),
-            float("inf"),
-            frequencyDomain=10000,
+            cluster_0,
+            readLatency=100,
+            writeLatency=100,
+            readThroughput=float("inf"),
+            writeThroughput=float("inf"),
+            frequency=10000,
         )
+        for pe in pe_list:
+            designer.connectPeToCom(pe, cl0_l2)
 
         # add second cluster with L2 cache
-        designer.addPeClusterForProcessor("cluster_1", processor_1, 2)
-        designer.addCommunicationResource(
+        cluster_1 = designer.addCluster("cluster_1", multi_cluster)
+        pe_list = list()
+        for i in range(2):
+            pe = designer.addPeToCluster(
+                cluster_1,
+                f"processor_{i:02d}_1",
+                processor_1.type,
+                processor_1.frequency_domain,
+                processor_1.power_model,
+                processor_1.context_load_cycles,
+                processor_1.context_store_cycles,
+            )
+            pe_list.append(pe)
+
+        cl1_l2 = designer.addStorage(
             "cl1_l2",
-            ["cluster_1"],
-            100,
-            100,
-            float("inf"),
-            float("inf"),
-            frequencyDomain=10000,
+            cluster_1,
+            readLatency=100,
+            writeLatency=100,
+            readThroughput=float("inf"),
+            writeThroughput=float("inf"),
+            frequency=10000,
         )
+        for pe in pe_list:
+            designer.connectPeToCom(pe, cl1_l2)
 
-        # connect both clusters via RAM
-        designer.addCommunicationResource(
+        # RAM connecting all clusters
+        ram = designer.addStorage(
             "RAM",
-            ["cluster_0", "cluster_1"],
+            multi_cluster,
             1000,
             1000,
             float("inf"),
             float("inf"),
-            frequencyDomain=1000,
+            frequency=1000,
         )
-
-        designer.finishElement()
+        designer.connectStorageLevels(cl0_l2, ram)
+        designer.connectStorageLevels(cl1_l2, ram)
