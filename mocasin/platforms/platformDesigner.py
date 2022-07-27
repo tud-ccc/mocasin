4f61848b40386b74b3b18f7d468a5a45916ba709
ratto <francesco.ratto@unica.it>
2022-07-27 15:57:04 +0200

    Updated docstring type of identifier

--- mocasin/platforms/platformDesigner.py
+++ mocasin/platforms/platformDesigner.py
@@ -80,7 +80,7 @@ class PlatformDesigner:
         """A new scope is opened and pushed on the stack.
 
         :param identifier: The identifier, the element can be addressed with.
-        :type identifier: int
+        :type identifier: str
         """
         if self.__symLibrary:
             if self._ag_hasSuperGraph():
@@ -142,7 +142,7 @@ class PlatformDesigner:
         """Creates a new cluster of processing elements on the platform.
 
         :param identifier: The identifier the cluster can be addressed within the currently active scope.
-        :type identifier: int
+        :type identifier: str
         :param name: The name of the processing elements.
         :type name: string
         :param amount: The amount of processing elements in the cluster.
@@ -195,7 +195,7 @@ class PlatformDesigner:
         """Creates a new cluster of processing elements on the platform.
 
         :param identifier: The identifier the cluster can be addressed within the currently active scope.
-        :type identifier: int
+        :type identifier: str
         :param processor: The mocasin Processor object which will be used for the cluster.
         :type processor: Processor
         :param amount: The amount of processing elements in the cluster.
@@ -275,7 +275,7 @@ class PlatformDesigner:
         """Adds a level 1 cache to each PE of the given cluster.
 
         :param identifier: The identifier of the cluster to which the cache will be added.
-        :type identifier: int
+        :type identifier: str
         :param readLatency: The read latency of the cache.
         :type readLatency: int
         :param writeLatency: The write latency of the cache.
@@ -352,7 +352,7 @@ class PlatformDesigner:
         :param name: The name of the storage
         :type name: String
         :param clusterIds: A list of identifiers for all clusters which will be connected.
-        :type clusterIds: list[int]
+        :type clusterIds: list[str]
         :param readLatency: The read latency of the communication resource.
         :type readLatency: int
         :param writeLatency: The write latency of the communication resource.
@@ -466,7 +466,7 @@ class PlatformDesigner:
         """Creates a network on chip topology for the given cluster.
 
         :param clusterIdentifier: The identifier of the cluster the network will be created for.
-        :type clusterIdentifier: int
+        :type clusterIdentifier: str
         :param networkName: The name of the network. (primitives belonging to the network will be named
                                 like this.
         :type networkName: String
@@ -766,7 +766,7 @@ class PlatformDesigner:
         """Returns a list of all processing elements contained in specified cluster.
 
         :param identifier: The identifier of the target cluster.
-        :type identifier: int
+        :type identifier: str
         :returns: A list of names of processing elements
         :rtype list[string]:
         """
